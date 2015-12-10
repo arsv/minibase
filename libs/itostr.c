cf788d5a36263d4fc4bf41daf898cfd05b884c58
Alex Suykov <alex.suykov@gmail.com>
2015-12-10 19:48:48 +0200

    fix itostr

--- libs/itostr.c
+++ libs/itostr.c
@@ -1,19 +1,26 @@
 #include "itostr.h"
+#include "strapp.h"
 
 char* itostr(char* buf, char* end, int num)
 {
-	char intbuf[8*sizeof(int)/3+2];
-	char* p = intbuf;
-	int i, l, n;
+	int len = 0;
+	int min = 0;
 
-	if(num < 0) { num = -num; *p++ = '-'; }
+	if(num < 0) { num = -num; min = 1; }
 
-	for(l = 1, n = num; n; l++)
+	int n;
+
+	for(len = 1, n = num; n; len++)
 		n /= 10;
 
-	for(i = 0, n = num; i < l; i++, n /= 10)
-		if(p + i < end)
-			p[i] = '0' + n % 10;
+	int i;
+	char* e = buf + len + min;
+	char* p = e - 1; /* len >= 1 so e > buf */
+	
+	for(i = 0; i < len; i++, p--)
+		if(p < end)
+			*p = '0' + n % 10;
+	if(min) *p = '-';
 
-	return (p + l < end ? p + l : end);
+	return e; 
 }
