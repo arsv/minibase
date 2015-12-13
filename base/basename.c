49f1f1bdd4e7c08a9569a1498a280b25333e6cf5
Alex Suykov <alex.suykov@gmail.com>
2015-12-13 02:07:49 +0200

    basename

---
+++ base/basename.c
@@ -0,0 +1,37 @@
+#include <sys/write.h>
+#include <strlen.h>
+
+/* XXX: should we support suffix-stripping and \0 termination? */
+
+void dumpbname(char* path)
+{
+	int len = strlen(path);
+	char* p;
+	char* q;
+
+	/* Strip trailing slashes */
+	for(p = path + len - 1; p >= path; p--)
+		if(*p == '/')
+			*p = '\0';
+		else
+			break;
+
+	/* Strip leading directories */
+	for(q = p = path; *p;)
+		if(*p++ == '/')
+			q = p;
+
+	*p++ = '\n';
+
+	syswrite(1, q, p - q);
+}
+
+int main(int argc, char** argv)
+{
+	int i;
+
+	for(i = 1; i < argc; i++)
+		dumpbname(argv[i]);
+
+	return 0;
+}
