e1640ee0d05f084801a4ba89ba282e00a67f33f2
Alex Suykov <alex.suykov@gmail.com>
2015-12-13 17:19:37 +0200

    rmmod to use proper cmdline switches

--- root/rmmod.c
+++ root/rmmod.c
@@ -4,6 +4,7 @@
 
 #include <null.h>
 #include <fail.h>
+#include <xchk.h>
 
 ERRTAG = "rmmod";
 ERRLIST = {
@@ -11,25 +12,37 @@ ERRLIST = {
 	REPORT(EPERM), REPORT(EWOULDBLOCK), RESTASNUMBERS
 };
 
+static int applyopts(int flags, const char* keys)
+{
+	const char *p;
+	char opt[] = "-?";
+
+	for(p = keys; *p; p++)
+		switch(*p) {
+			case 'f': flags |= O_TRUNC; break;
+			case 'w': flags &= ~O_NONBLOCK; break;
+			default:
+				opt[1] = *p;
+				fail("unknown option", opt, 0);
+		}
+
+	return flags;
+}
+
 int main(int argc, char** argv)
 {
 	int flags = O_NONBLOCK;
+	int i = 1;
 
-	if(argc < 2)
+	if(i < argc && argv[i][0] == '-')
+		flags = applyopts(flags, argv[i++] + 1);
+
+	if(i >= argc)
 		fail("module name required", NULL, 0);
-	if(argc > 2)
-		fail("cannot remove several modules", NULL, 0);
 
-	const char* mod = argv[1] + 1;
-	switch(argv[1][0]) {
-		case '-': flags |=  O_TRUNC; break;
-		case '+': flags &= ~O_NONBLOCK; break;
-		default: mod--;
-	}
+	for(; i < argc; i++)
+		xchk( sysdeletemodule(argv[i], flags),
+			"cannot remove", argv[i] );
 
-	long ret = sysdeletemodule(mod, flags);
-
-	if(ret >= 0) return 0;
-
-	fail("cannot remove", mod, -ret);
+	return 0;
 }
