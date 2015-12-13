cf0711977a54e802776f8f91dd0f3994e709a5cd
Alex Suykov <alex.suykov@gmail.com>
2015-12-13 17:19:11 +0200

    access bits in stat.h

--- libs/bits/stat.h
+++ libs/bits/stat.h
@@ -1,7 +1,7 @@
 #ifndef __BITS_STAT_H__
 #define __BITS_STAT_H__
 
-#define S_IFMT  00170000
+#define S_IFMT   0170000
 #define S_IFSOCK 0140000
 #define S_IFLNK	 0120000
 #define S_IFREG  0100000
@@ -12,6 +12,21 @@
 #define S_ISUID  0004000
 #define S_ISGID  0002000
 #define S_ISVTX  0001000
+
+#define S_IRWXU  0000700
+#define S_IRUSR  0000400
+#define S_IWUSR  0000200
+#define S_IXUSR  0000100
+
+#define S_IRWXG  0000070
+#define S_IRGRP  0000040
+#define S_IWGRP  0000020
+#define S_IXGRP  0000010
+
+#define S_IRWXO  0000007
+#define S_IROTH  0000004
+#define S_IWOTH  0000002
+#define S_IXOTH  0000001
 
 #define S_ISLNK(m)	(((m) & S_IFMT) == S_IFLNK)
 #define S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)
