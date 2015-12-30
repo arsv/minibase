6d72b0b346a0f50d15d99ca8d46992fd16398f1f
Alex Suykov <alex.suykov@gmail.com>
2015-12-30 13:13:49 +0200

    mountvfs: move-mounts

--- admin/mountvfs.c
+++ admin/mountvfs.c
@@ -1,10 +1,16 @@
 #include <bits/errno.h>
 #include <bits/mount.h>
+#include <bits/statfs.h>
 #include <sys/mount.h>
+#include <sys/statfs.h>
 
+#include <argbits.h>
 #include <strcmp.h>
+#include <strlen.h>
+#include <memcpy.h>
 #include <null.h>
 #include <fail.h>
+#include <xchk.h>
 
 ERRTAG = "mountvfs";
 ERRLIST = {
@@ -41,32 +47,103 @@ static const struct mount {
 
 static void mountentry(const struct mount* p)
 {
-	int ret = sysmount("none", p->point, p->type, p->flags, p->opts);
-
-	if(ret >= 0) return;
-
-	fail("cannot mount", p->point, -ret);
+	xchk( sysmount("none", p->point, p->type, p->flags, p->opts),
+		"cannot mount", p->point);
 }
 
-static void mountbyname(const char* name)
+static const struct mount* findentry(const char* name)
 {
 	const struct mount* p;
 
 	for(p = vfstab; p->name; p++)
 		if(!strcmp(p->name, name))
-			return mountentry(p);
+			return p;
 
 	fail("unknown filesystem", name, 0);
 }
 
+static void mounttags(int i, int argc, char** argv)
+{
+	if(i >= argc)
+		fail("nothing to mount", NULL, 0);
+	for(; i < argc; i++)
+		mountentry(findentry(argv[i]));
+}
+
+static long movemount(char* target, char* source)
+{
+	long slen = strlen(source);
+	long tlen = strlen(target);
+	char fulltarget[slen + tlen + 2];
+
+	memcpy(fulltarget, target, tlen);
+	memcpy(fulltarget + tlen, source, slen);
+	fulltarget[tlen+slen] = '\0';
+
+	return sysmount(source, fulltarget, NULL, MS_MOVE, NULL);
+}
+
+static void chkmove(char* target, const struct mount* p)
+{
+	xchk(movemount(target, p->point), "cannot move", p->point);
+}
+
+static void trymove(char* target, const struct mount* p, struct statfs* rst)
+{
+	struct statfs pst;
+	long ret;
+
+	if((ret = sysstatfs(p->point, &pst)) < 0)
+		return;
+	if(pst.f_fsid == rst->f_fsid)
+		return;
+
+	ret = movemount(target, p->point);
+
+	if(ret > 0 || ret == -EINVAL)
+		return;
+
+	warn("cannot move", p->point, -ret);
+}
+
+static void movetags(int i, int argc, char** argv)
+{
+	if(i >= argc)
+		fail("target directory expected", NULL, 0);
+
+	char* target = argv[i++];
+
+	if(i < argc) {
+		while(i < argc)
+			chkmove(target, findentry(argv[i++]));
+	} else {
+		long ret;
+		const struct mount* p;
+		struct statfs st;
+
+		if((ret = sysstatfs("/", &st)) < 0)
+			fail("cannot statfs", "/", -ret);
+
+		for(p = vfstab; p->name; p++)
+			trymove(target, p, &st);
+	}
+}
+
+#define OPTS "m"
+#define OPT_m (1<<0)
+
 int main(int argc, char** argv)
 {
-	int i;
+	int i = 1;
+	int opts = 0;
 
-	if(argc < 2)
-		fail("nothing to mount", NULL, 0);
-	for(i = 1; i < argc; i++)
-		mountbyname(argv[i]);
+	if(i < argc && argv[i][0] == '-')
+		opts = argbits(OPTS, argv[i++] + 1);
+
+	if(opts & OPT_m)
+		movetags(i, argc, argv);
+	else
+		mounttags(i, argc, argv);
 
 	return 0;
 }
