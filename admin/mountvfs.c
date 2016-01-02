c5168fe21618777826ab8e08b501288149a07ed6
Alex Suykov <alex.suykov@gmail.com>
2016-01-03 01:02:42 +0200

    mountvfs: -u and mkdir

--- admin/mountvfs.c
+++ admin/mountvfs.c
@@ -2,6 +2,8 @@
 #include <bits/mount.h>
 #include <bits/statfs.h>
 #include <sys/mount.h>
+#include <sys/umount.h>
+#include <sys/mkdir.h>
 #include <sys/statfs.h>
 
 #include <argbits.h>
@@ -30,7 +32,7 @@ static const struct mount {
 	char* point;
 	long flags;
 	char* opts;
-} vfstab[] = {
+} const vfstab[] = {
  { "dev",	"devtmpfs",	"/dev",				SX,	NULL },
  { "pts",	"devpts",	"/dev/pts",			SX,	NULL },
  { "shm",	"tmpfs",	"/dev/shm",			SX,	NULL },
@@ -45,21 +47,50 @@ static const struct mount {
  { NULL }
 };
 
-static void mountentry(const struct mount* p)
-{
-	xchk( sysmount("none", p->point, p->type, p->flags, p->opts),
-		"cannot mount", p->point);
-}
+static const int nvfs = sizeof(vfstab) / sizeof(*vfstab);
 
 static const struct mount* findentry(const char* name)
 {
 	const struct mount* p;
 
-	for(p = vfstab; p->name; p++)
+	for(p = vfstab; p < vfstab + nvfs; p++)
 		if(!strcmp(p->name, name))
 			return p;
 
 	fail("unknown filesystem", name, 0);
+}
+
+/* Mounting. There is no default run (w/o arguments),
+   the tags must always be supplied.
+
+   For some cases we need, and for most we can just in case
+   try to make the mountpoint if does not exist.
+
+   For totally vain reasons, namely to avoid one redundant call
+   in the better case, we attempt to mountentry() and only if
+   that fails do mkdir-mountentry sequence. The alternative is
+   doing mkdir first, expecting EEXIST. */
+
+static long mountentry(const struct mount* p)
+{
+	return sysmount("none", p->point, p->type, p->flags, p->opts);
+}
+
+static void chkmount(const struct mount* p)
+{
+	long ret;
+
+	if((ret = mountentry(p)) >= 0)
+		return;
+	if(ret != -ENOENT)
+		goto err;
+
+	if((ret = sysmkdir(p->point, 0755)) < 0)
+		goto err;
+	if((ret = mountentry(p)) >= 0)
+		return;
+
+err:	fail("cannot mount", p->point, -ret);
 }
 
 static void mounttags(int i, int argc, char** argv)
@@ -67,8 +98,17 @@ static void mounttags(int i, int argc, char** argv)
 	if(i >= argc)
 		fail("nothing to mount", NULL, 0);
 	for(; i < argc; i++)
-		mountentry(findentry(argv[i]));
+		chkmount(findentry(argv[i]));
 }
+
+/* Moving. No tags means "move all mounted VFSes".
+
+   We tell mounted from not-mounted by comparing statfs.f_fsid.
+   The test is not exactly reliable, and there are nested entries,
+   so error reporting is somewhat relaxed.
+
+   For explicitly specified tags, any errors are reported.
+   The assumption is that if the user listed a dir, it must be there. */
 
 static long movemount(char* target, char* source)
 {
@@ -124,13 +164,43 @@ static void movetags(int i, int argc, char** argv)
 		if((ret = sysstatfs("/", &st)) < 0)
 			fail("cannot statfs", "/", -ret);
 
-		for(p = vfstab; p->name; p++)
+		for(p = vfstab; p < vfstab + nvfs; p++)
 			trymove(target, p, &st);
 	}
 }
 
-#define OPTS "m"
+/* Unmounting. For no-arg case, this is done in reverse order,
+   so that /dev/pts is umounted before /dev
+
+   We do not attempt to check whether the point is mounted,
+   that's at least one syscall per mountpoint and we can just
+   as well by simply calling sysumount(). */
+
+static long umountentry(const struct mount* p)
+{
+	return sysumount(p->point, MNT_DETACH);
+}
+
+static void chkumount(const struct mount* p)
+{
+	xchk(umountentry(p), "cannot umount", p->point);
+}
+
+static void umounttags(int i, int argc, char** argv)
+{
+	const struct mount* p;
+
+	if(i < argc)
+		while(i < argc)
+			chkumount(findentry(argv[i++]));
+	else
+		for(p = vfstab + nvfs - 1; p >= vfstab; p--)
+			umountentry(p);
+}
+
+#define OPTS "mu"
 #define OPT_m (1<<0)
+#define OPT_u (1<<1)
 
 int main(int argc, char** argv)
 {
@@ -140,7 +210,12 @@ int main(int argc, char** argv)
 	if(i < argc && argv[i][0] == '-')
 		opts = argbits(OPTS, argv[i++] + 1);
 
-	if(opts & OPT_m)
+	if((opts & OPT_m) && (opts & OPT_u))
+		fail("cannot use -u and -m at the same time", NULL, 0);
+
+	if(opts & OPT_u)
+		umounttags(i, argc, argv);
+	else if(opts & OPT_m)
 		movetags(i, argc, argv);
 	else
 		mounttags(i, argc, argv);
