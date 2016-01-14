8f09252afa4ec75150ebc7be9a3435526cd8fe38
Alex Suykov <alex.suykov@gmail.com>
2016-01-14 23:55:18 +0200

    x86_64: proper struct stat
    No idea where do they define it. It's not the common struct
    from kernel uapi headers that's for sure.

--- lib/arch/x86_64/bits/stat.h
+++ lib/arch/x86_64/bits/stat.h
@@ -4,12 +4,12 @@
 struct stat {
 	unsigned long	st_dev;		/* Device.  */
 	unsigned long	st_ino;		/* File serial number.  */
-	unsigned int	st_mode;	/* File mode.  */
 	unsigned int	st_nlink;	/* Link count.  */
+	unsigned int	__pad0;		/* File mode.  */
+	unsigned int	st_mode;	/* File mode.  */
 	unsigned int	st_uid;		/* User ID of the file's owner.  */
 	unsigned int	st_gid;		/* Group ID of the file's group. */
 	unsigned long	st_rdev;	/* Device number, if device.  */
-	unsigned long	__pad1;
 	long		st_size;	/* Size of file, in bytes.  */
 	int		st_blksize;	/* Optimal block size for I/O.  */
 	int		__pad2;
