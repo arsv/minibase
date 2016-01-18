28b8476a5b0f2a541b5f943380204bc5b92a7b62
Alex Suykov <alex.suykov@gmail.com>
2016-01-18 03:33:29 +0200

    sleep

---
+++ src/misc/sleep.c
@@ -0,0 +1,113 @@
+#include <sys/nanosleep.h>
+#include <sys/write.h>
+#include <sys/_exit.h>
+
+#include <strlen.h>
+#include <memcpy.h>
+#include <null.h>
+
+#define NANOFRAC 1000000000 /* nanoseconds in a second */
+
+/* There's no errno reporting in sleep, so no point in bringing fail(). */
+
+static void fail2(const char* msg, const char* obj)
+{
+	unsigned msglen = strlen(msg);
+	unsigned objlen = obj ? strlen(obj) : 0;
+	char buf[msglen+objlen+2];
+	char* p = buf;
+
+	memcpy(p, msg, msglen); p += msglen;
+	if(!obj) goto nl;
+
+	*p++ = ' ';
+	memcpy(p, obj, objlen); p += objlen;
+
+nl:	*p++ = '\n';
+	syswrite(2, buf, p - buf);
+	_exit(255);
+}
+
+/* Valid time specs:
+   	123      seconds
+	123.456  seconds, with non-zero nanoseconds part
+	12m      minutes
+	3h       hours
+   Suffixing seconds with "s" is also allowed for consistency,
+   but "ms" and "ns" are not accepted. */
+
+static void parsetime(struct timespec* sp, const char* str)
+{
+	unsigned long sec = 0;
+	unsigned long nsec = 0;
+	unsigned long nmul = NANOFRAC;
+	const char* p;
+	int d;
+
+	if(!*str) fail2("invalid time spec", str);
+
+	/* Integer part */
+	for(p = str; *p; p++)
+		if(*p >= '0' && (d = *p - '0') < 10)
+			sec = sec * 10 + d;
+		else
+			break;
+
+	/* Suffix (including eos and '.') */
+	switch(*p) {
+		case 'd': sec *= 24;
+		case 'h': sec *= 60;
+		case 'm': sec *= 60;
+		case 's':
+		case '\0':
+		case '.': break;
+		default: fail2("invalid time suffix", str);
+	}
+	
+	/* Fractional part, if any */
+	if(*p == '.') for(p++; *p; p++) {
+		if(*p >= '0' && (d = *p - '0') < 10) {
+			if(nmul >= 10) {
+				nsec = nsec * 10 + d;
+				nmul /= 10;
+			}
+		} else {
+			fail2("invalid second fraction", str);
+		}
+	};
+
+	sp->tv_sec = sec;
+	sp->tv_nsec = nsec*nmul;
+}
+
+/* Several arguments are summed up:
+
+   	sleep 3m 15s -> sleep(195)
+
+   Kind of pointless, but since we support suffixes, well why not. */
+
+static void addtime(struct timespec* sp, const char* str)
+{
+	struct timespec ts = { 0, 0 };
+
+	parsetime(&ts, str);
+
+	sp->tv_nsec += ts.tv_nsec;
+	long rem = (sp->tv_nsec > NANOFRAC-1) ? sp->tv_nsec / NANOFRAC : 0;
+	sp->tv_sec += ts.tv_sec + rem;
+}
+
+int main(int argc, char** argv)
+{
+	int i = 1;
+	struct timespec sp = { 0, 0 };
+
+	if(i >= argc)
+		fail2("missing argument", NULL);
+	else for(; i < argc; i++)
+		addtime(&sp, argv[i]);
+
+	long ret = sysnanosleep(&sp, NULL);
+
+	return (ret < 0 ? -1 : 0);
+}
