2bb64282abc85ac84a99a3036bbfb1bba9def49e
Alex Suykov <alex.suykov@gmail.com>
2016-03-22 15:14:47 +0200

    ls: symlink-to-dir handling

--- src/ls.c
+++ src/ls.c
@@ -24,13 +24,16 @@ ERRLIST = {
 #define MAYBEDIR  0
 #define MUSTBEDIR 1
 
-#define OPTS "aubrey"
+#define DT_LNK_DIR 71	/* symlink pointing to a dir, custom value */
+
+#define OPTS "aubreyw"
 #define OPT_a (1<<0)	/* show all files, including hidden ones */
 #define OPT_u (1<<1)	/* uniform listing, dirs and filex intermixed */
 #define OPT_b (1<<2)	/* basename listing, do not prepend argument */
 #define OPT_r (1<<3)	/* recurse into subdirectories */
 #define OPT_e (1<<4)	/* list leaf entries (non-dirs) only */
 #define OPT_y (1<<5)	/* list symlinks as files, regardless of target */
+#define OPT_w (1<<6)	/* follow symlinks */
 
 #define SET_stat (1<<16) /* do stat() entries */
 
@@ -144,8 +147,8 @@ static void statidx(struct idxent* idx, int nument, int fd, int opts)
 {
 	struct idxent* p;
 	struct stat st;
-	int follow = !(opts & OPT_y);
-	int flags = AT_NO_AUTOMOUNT | (!follow ? AT_SYMLINK_NOFOLLOW : 0);
+	int flags1 = AT_NO_AUTOMOUNT;
+	int flags2 = AT_NO_AUTOMOUNT | AT_SYMLINK_NOFOLLOW;
 
 	if(!(opts & SET_stat))
 		return;
@@ -153,29 +156,38 @@ static void statidx(struct idxent* idx, int nument, int fd, int opts)
 	for(p = idx; p < idx + nument; p++) {
 		int type = p->de->d_type;
 
-		if(type == DT_UNKNOWN)
+		if(type != DT_UNKNOWN)
 			;
-		else if((type == DT_LNK) && follow)
-			;
-		else continue;
+		else if(sysfstatat(fd, p->de->d_name, &st, flags1) < 0)
+			continue;
+		else if(S_ISDIR(st.st_mode))
+			type = DT_DIR;
+		else if(S_ISLNK(st.st_mode))
+			type = DT_LNK;
+		else
+			type = DT_REG; /* neither DIR nor LNK nor UNKNOWN */
 
-		if(sysfstatat(fd, p->de->d_name, &st, flags) < 0)
+		p->de->d_type = type;
+
+		if(type != DT_LNK)
+			continue;
+		if(opts & OPT_y)
 			continue;
 
-		if(S_ISDIR(st.st_mode))
-			p->de->d_type = DT_DIR;
-		else if(S_ISLNK(st.st_mode))
-			p->de->d_type = DT_LNK;
-		else
-			p->de->d_type = DT_REG; /* neither DIR nor UNKNOWN */
+		if(sysfstatat(fd, p->de->d_name, &st, flags2) < 0)
+			continue;
+		if(S_ISLNK(st.st_mode))
+			p->de->d_type = DT_LNK_DIR;
 	}
 }
 
 static int cmpidx(struct idxent* a, struct idxent* b, int opts)
 {
+	int dirtype(int t) { return (t == DT_DIR || t == DT_LNK_DIR); };
+
 	if(!(opts & OPT_u)) {
-		int dira = (a->de->d_type == DT_DIR);
-		int dirb = (b->de->d_type == DT_DIR);
+		int dira = dirtype(a->de->d_type);
+		int dirb = dirtype(b->de->d_type);
 
 		if(dira && !dirb)
 			return -1;
@@ -227,8 +239,10 @@ static void dumpentry(struct topctx* tc, struct dirctx* dc, struct dirent64* de)
 
 	bufout(bo, name, strlen(name));
 
-	if(type == DT_DIR)
-		bufout(bo, "/", 1);
+	if(type == DT_LNK_DIR)
+		bufout(bo, "//", 2);
+	else if(type == DT_DIR)
+		bufout(bo, "//", 1);
 
 	bufout(bo, "\n", 1);
 }
@@ -256,6 +270,8 @@ static void dumplist(struct topctx* tc, struct dirctx* dc,
 		if(!(opts & OPT_r))
 			continue;
 		if(type == DT_DIR)
+			recurse(tc, dc, name, MUSTBEDIR);
+		else if(type == DT_LNK_DIR && (opts & OPT_w))
 			recurse(tc, dc, name, MUSTBEDIR);
 		else if(type == DT_UNKNOWN)
 			recurse(tc, dc, name, MAYBEDIR);
@@ -285,10 +301,14 @@ static void list(struct topctx* tc, const char* realpath, const char* showpath, 
 	struct dataseg* ds = &(tc->ds);
 	struct dirctx dc;
 	int opts = tc->opts;
+	int flags = O_RDONLY | O_DIRECTORY;
+
+	if(!strict && !(opts & OPT_w))
+		flags |= O_NOFOLLOW;
 
 	if(tc->fd >= 0) sysclose(tc->fd); /* delayed close */
 
-	if((tc->fd = sysopen(realpath, O_RDONLY | O_DIRECTORY)) < 0) {
+	if((tc->fd = sysopen(realpath, flags)) < 0) {
 		if(strict && tc->fd == ENOTDIR)
 			return;
 		else
