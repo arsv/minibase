62818f4e412cc702ebc0a399f1f3e4067d269ca3
Alex Suykov <alex.suykov@gmail.com>
2016-03-22 03:06:32 +0200

    ls: recursive listing

--- src/ls.c
+++ src/ls.c
@@ -21,10 +21,14 @@ ERRLIST = {
 
 #define PAGE 4096
 
-#define OPTS "aub"
+#define MAYBEDIR  0
+#define MUSTBEDIR 1
+
+#define OPTS "aubr"
 #define OPT_a (1<<0)	/* show all files, including hidden ones */
 #define OPT_u (1<<1)	/* uniform listing, dirs and filex intermixed */
 #define OPT_b (1<<2)	/* basename listing, do not prepend argument */
+#define OPT_r (1<<3)	/* recurse into subdirectories */
 
 struct dataseg {
 	void* base;
@@ -50,12 +54,13 @@ struct dirctx {
 
 char output[PAGE];
 
-static void init(struct topctx* tc)
+static void init(struct topctx* tc, int opts)
 {
 	void* brk = (void*)xchk(sysbrk(0), "brk", NULL);
 	void* end = (void*)xchk(sysbrk(brk + PAGE), "brk", NULL);
 
 	tc->fd = -1;
+	tc->opts = opts;
 
 	tc->ds.base = brk;
 	tc->ds.end = end;
@@ -128,7 +133,7 @@ static void statidx(struct idxent* idx, int nument, int fd, int opts)
 	struct stat st;
 	const int flags = AT_NO_AUTOMOUNT;
 
-	if(opts & OPT_u)
+	if(opts & (OPT_u | OPT_r))
 		return; /* no need to stat anything for uniform lists */
 
 	for(p = idx; p < idx + nument; p++) {
@@ -140,6 +145,8 @@ static void statidx(struct idxent* idx, int nument, int fd, int opts)
 
 		if(S_ISDIR(st.st_mode))
 			p->de->d_type = DT_DIR;
+		else
+			p->de->d_type = DT_REG; /* neither DIR nor UNKNOWN */
 	}
 }
 
@@ -171,7 +178,24 @@ static int dotddot(const char* name)
 	return 0;
 }
 
-static void dumplist(struct topctx* tc, struct dirctx* dc, struct idxent* idx, int nument)
+static void list(struct topctx* tc, const char* realpath, const char* showpath, int strict);
+
+static void recurse(struct topctx* tc, struct dirctx* dc,
+		const char* name, int len, int strict)
+{
+	struct dataseg* ds = &(tc->ds);
+	char* fullname = (char*)alloc(ds, dc->len + 1 + len + 1);
+	char* p = fullname;
+
+	memcpy(p, dc->dir, dc->len); p += dc->len;
+	if(p > fullname && *(p-1) != '/') *p++ = '/';
+	memcpy(p, name, len); p += len; *p++ = '\0';
+
+	list(tc, fullname, fullname, strict);
+}
+
+static void dumplist(struct topctx* tc, struct dirctx* dc,
+		struct idxent* idx, int nument)
 {
 	struct idxent* p;
 	struct bufout* bo = &(tc->bo);
@@ -186,12 +210,24 @@ static void dumplist(struct topctx* tc, struct dirctx* dc, struct idxent* idx, i
 		if(dotddot(name))
 			continue;
 
+		int namelen = strlen(name);
+
 		if(dc->len)
 			bufout(bo, dc->dir, dc->len);
-		bufout(bo, name, strlen(name));
+
+		bufout(bo, name, namelen);
+
 		if(type == DT_DIR)
 			bufout(bo, "/", 1);
+
 		bufout(bo, "\n", 1);
+
+		if(!(opts & OPT_r))
+			continue;
+		if(type == DT_DIR)
+			recurse(tc, dc, name, namelen, MUSTBEDIR);
+		else if(type == DT_UNKNOWN)
+			recurse(tc, dc, name, namelen, MAYBEDIR);
 	}
 }
 
@@ -213,15 +249,20 @@ static void makedirctx(struct topctx* tc, struct dirctx* dc, const char* path)
 	}
 }
 
-static void list(struct topctx* tc, const char* realpath, const char* showpath)
+static void list(struct topctx* tc, const char* realpath, const char* showpath, int strict)
 {
 	struct dataseg* ds = &(tc->ds);
 	struct dirctx dc;
 	int opts = tc->opts;
 
 	if(tc->fd >= 0) sysclose(tc->fd); /* delayed close */
-	tc->fd = xchk(sysopen(realpath, O_RDONLY | O_DIRECTORY),
-			"cannot open", realpath);
+
+	if((tc->fd = sysopen(realpath, O_RDONLY | O_DIRECTORY)) < 0) {
+		if(strict && tc->fd == ENOTDIR)
+			return;
+		else
+			fail("cannot open", realpath, tc->fd);
+	}
 
 	void* oldptr = ds->ptr;		/* start ds frame */
 
@@ -244,19 +285,18 @@ static void list(struct topctx* tc, const char* realpath, const char* showpath)
 int main(int argc, char** argv)
 {
 	struct topctx tc;
+	int opts = 0;
 	int i = 1;
 
 	if(i < argc && argv[i][0] == '-')
-		tc.opts = argbits(OPTS, argv[i++] + 1);
-	else
-		tc.opts = 0;
+		opts = argbits(OPTS, argv[i++] + 1);
 
-	init(&tc);
+	init(&tc, opts);
 
 	if(i >= argc)
-		list(&tc, ".", NULL);
+		list(&tc, ".", NULL, MUSTBEDIR);
 	else for(; i < argc; i++)
-		list(&tc, argv[i], (tc.opts & OPT_b) ? NULL : argv[i]);
+		list(&tc, argv[i], (opts & OPT_b) ? NULL : argv[i], MUSTBEDIR);
 
 	fini(&tc);
 
