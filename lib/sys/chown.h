8e2e1361129876d9656c42229c5adb824013edce
Alex Suykov <alex.suykov@gmail.com>
2016-08-11 21:05:17 +0300

    use at-calls for chmod/chown/readlink if available
    Modern asm-generic based arches may not implement non-at variants.
    Currently affects aarch64 and riscv.

--- lib/sys/chown.h
+++ lib/sys/chown.h
@@ -2,5 +2,9 @@
 
 inline static long syschown(const char* filename, int uid, int gid)
 {
+#ifdef __NR_fchownat
+	return syscall5(__NR_fchownat, AT_FDCWD, (long)filename, uid, gid, 0);
+#else
 	return syscall3(__NR_chown, (long)filename, uid, gid);
+#endif
 }
--- lib/sys/readlink.h
+++ lib/sys/readlink.h
@@ -1,6 +1,11 @@
 #include <syscall.h>
+#include <bits/fcntl.h>
 
 inline static long sysreadlink(const char* filename, char* buf, long len)
 {
+#ifdef __NR_readlinkat
+	return syscall4(__NR_readlinkat, AT_FDCWD, (long)filename, (long)buf, len);
+#else
 	return syscall3(__NR_readlink, (long)filename, (long)buf, len);
+#endif
 }
