fb9147096db36436283ee207188f9662c461d1eb
Alex Suykov <alex.suykov@gmail.com>
2016-12-18 23:03:18 +0200

    lib: (u)int16_t

--- lib/arch/x86_64/bits/ints.h
+++ lib/arch/x86_64/bits/ints.h
@@ -3,9 +3,11 @@
 
 #define BITS 64
 
-typedef signed int     int32_t;
-typedef signed long    int64_t;
-typedef unsigned int  uint32_t;
-typedef unsigned long uint64_t;
+typedef signed short    int16_t;
+typedef signed int      int32_t;
+typedef signed long     int64_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef unsigned long  uint64_t;
 
 #endif
