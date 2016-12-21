c34af7731a2ff4b870646b73d6f5dc1130cdeb93
Alex Suykov <alex.suykov@gmail.com>
2016-12-21 02:01:10 +0200

    lib: remove pointless struct sockaddr from sysbind()

--- lib/sys/bind.h
+++ lib/sys/bind.h
@@ -1,8 +1,6 @@
 #include <syscall.h>
 
-struct sockaddr;
-
-inline static long sysbind(int fd, const struct sockaddr* addr, int len)
+inline static long sysbind(int fd, void* addr, int len)
 {
 	return syscall3(__NR_bind, fd, (long)addr, len);
 }
