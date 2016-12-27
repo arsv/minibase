b0d6cd6e8f20e2b4792faa2ab86f16af15807b27
Alex Suykov <alex.suykov@gmail.com>
2016-12-27 16:46:19 +0200

    lib: do not use struct sockaddr* arguments
    Those are never actually struct sockaddr but sockaddr_in
    or sockaddr_un, not a situation C type system handles well.
    
    Just using void* means less type casting.

--- lib/sys/connect.h
+++ lib/sys/connect.h
@@ -1,8 +1,6 @@
 #include <syscall.h>
 
-struct sockaddr;
-
-inline static long sysconnect(int fd, const struct sockaddr* addr, int len)
+inline static long sysconnect(int fd, void* addr, int len)
 {
 	return syscall3(__NR_connect, fd, (long)addr, len);
 }
