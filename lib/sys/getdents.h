be73384292d076695351920e9291eadcb43c372d
Alex Suykov <alex.suykov@gmail.com>
2016-12-28 13:18:10 +0200

    lib: void* argument for getdents()
    The buffer supplied there is *never* of struct dirent type,
    which is variable-length. It is supposed to be a plain byte buffer.

--- lib/sys/getdents.h
+++ lib/sys/getdents.h
@@ -1,7 +1,7 @@
 #include <syscall.h>
 #include <bits/dirent.h>
 
-inline static long sysgetdents64(int fd, struct dirent64* dp, int count)
+inline static long sysgetdents64(int fd, void* dp, int count)
 {
 	return syscall3(__NR_getdents64, fd, (long)dp, count);
 }
