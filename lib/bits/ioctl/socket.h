31192553896883dc1da78a80469686d3efb8ffe5
Alex Suykov <alex.suykov@gmail.com>
2016-12-30 22:48:06 +0200

    lib: bits/ioctl/socket

---
+++ lib/bits/ioctl/socket.h
@@ -0,0 +1,16 @@
+#include <bits/socket.h>
+
+#define SIOCGIFNAME	0x8910
+#define SIOCGIFHWADDR	0x8927
+#define SIOCGIFINDEX	0x8933
+
+#define IFNAMESIZ 16
+
+struct ifreq {
+	char name[IFNAMESIZ];
+	union {
+		int ival;
+		struct sockaddr addr;
+		char _[64]; /* 34, plus some slack */
+	};
+};
