2d48a8713925a3eda660c88dc4d6d3ac0639460d
Alex Suykov <alex.suykov@gmail.com>
2017-01-23 19:07:52 +0200

    lib: mremap constants

--- lib/bits/mman.h
+++ lib/bits/mman.h
@@ -1,14 +1,17 @@
 #ifndef __BITS_MMAN_H__
 #define __BITS_MMAN_H__
 
-#define PROT_READ	(1<<0)		/* page can be read */
-#define PROT_WRITE	(1<<1)		/* page can be written */
-#define PROT_EXEC	(1<<2)		/* page can be executed */
+#define PROT_READ       (1<<0)
+#define PROT_WRITE      (1<<1)
+#define PROT_EXEC       (1<<2)
 
-#define MAP_SHARED	(1<<0)		/* Share changes */
-#define MAP_PRIVATE	(1<<1)		/* Changes are private */
-#define MAP_FIXED	(1<<4)		/* Interpret addr exactly */
-#define MAP_ANONYMOUS	(1<<5)		/* don't use a file */
+#define MAP_SHARED      (1<<0)
+#define MAP_PRIVATE     (1<<1)
+#define MAP_FIXED       (1<<4)
+#define MAP_ANONYMOUS   (1<<5)
+
+#define MREMAP_MAYMOVE  (1<<0)
+#define MREMAP_FIXED    (1<<1)
 
 #define MMAPERROR(ret) ( ((ret) < 0) && ((ret) > -2048) )
 
