7de27b37c9d9e3d93a6492522cd513d95318d517
Alex Suykov <alex.suykov@gmail.com>
2017-02-01 17:01:28 +0200

    lib: drm and tty ioctls for vtmux

---
+++ lib/bits/ioctl/drm.h
@@ -0,0 +1,4 @@
+#include <bits/ioctl.h>
+
+#define DRM_IOCTL_SET_MASTER            _IO('d', 0x1e)
+#define DRM_IOCTL_DROP_MASTER           _IO('d', 0x1f)
---
+++ lib/bits/ioctl/tty.h
@@ -0,0 +1,30 @@
+#define VT_OPENQRY	0x5600	/* find available vt */
+
+struct vt_mode {
+	char mode;		/* vt mode */
+	char waitv;		/* if set, hang on writes if not active */
+	short relsig;		/* signal to raise on release req */
+	short acqsig;		/* signal to raise on acquisition */
+	short frsig;		/* unused (set to 0) */
+};
+
+#define VT_GETMODE	0x5601	/* get mode of active vt */
+#define VT_SETMODE	0x5602	/* set mode of active vt */
+#define		VT_AUTO		0x00	/* auto vt switching */
+#define		VT_PROCESS	0x01	/* process controls switching */
+#define		VT_ACKACQ	0x02	/* acknowledge switch */
+
+struct vt_stat {
+	unsigned short active;	/* active vt */
+	unsigned short signal;	/* signal to send */
+	unsigned short state;		/* vt bitmask */
+};
+
+#define VT_GETSTATE	0x5603	/* get global vt state info */
+#define VT_SENDSIG	0x5604	/* signal to send to bitmask of vts */
+
+#define VT_RELDISP	0x5605	/* release display */
+
+#define VT_ACTIVATE	0x5606	/* make vt active */
+#define VT_WAITACTIVE	0x5607	/* wait for vt active */
+#define VT_DISALLOCATE	0x5608  /* free memory associated to vt */
