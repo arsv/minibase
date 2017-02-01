e4cbe4f1d3289aac7706450391d6f05e7c00300f
Alex Suykov <alex.suykov@gmail.com>
2017-02-01 08:22:29 +0200

    lib: no-arg ioctl, _IO

--- lib/bits/ioctl.h
+++ lib/bits/ioctl.h
@@ -7,6 +7,7 @@
 #define _IOC(dir,type,nr,size) \
 	( ((dir)<<30) | ((type)<<8) | (nr) | ((size)<<16) )
 
+#define _IO(type,nr)		_IOC(0,(type),(nr),0)
 #define _IOR(type,nr,size)	_IOC(_IOC_R,(type),(nr),sizeof(size))
 #define _IOW(type,nr,size)	_IOC(_IOC_W,(type),(nr),sizeof(size))
 #define _IOWR(type,nr,size)	_IOC(_IOC_R|_IOC_W,(type),(nr),sizeof(size))
