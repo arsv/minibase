f9d14d2717c0a40e3b1d35a3199244d4e7a8c927
Alex Suykov <alex.suykov@gmail.com>
2017-02-15 01:05:47 +0200

    ip4cfg: merge utils.c in
    None of that ended up in ip4info, and there are no other
    uses for it atm.

--- src/net/ip4cfg.c
+++ src/net/ip4cfg.c
@@ -11,8 +11,6 @@
 #include <util.h>
 #include <fail.h>
 
-#include "utils.h"
-
 ERRTAG = "ip4cfg";
 ERRLIST = {
 	REPORT(EPERM), REPORT(EBUSY), REPORT(ENOENT), REPORT(EBADF),
@@ -28,6 +26,73 @@ ERRLIST = {
 
 char txbuf[1024];
 char rxbuf[3*1024];
+
+char* parseip(char* p, uint8_t ip[4])
+{
+	int i, n;
+
+	for(i = 0; i < 4; i++) {
+		if(i && *p != '.')
+			return NULL;
+		if(i) p++;
+
+		if(!(p = parseint(p, &n)))
+			return NULL;
+		if(n < 0 || n > 255)
+			return NULL;
+
+		ip[i] = n;
+	}
+
+	return p;
+}
+
+char* parseipmask(char* p, uint8_t ip[5])
+{
+	int n;
+
+	if(!(p = parseip(p, ip)))
+		return p;
+
+	if(*p == '/') {
+		p = parseint(p+1, &n);
+		if(n < 0 || n > 32) return NULL;
+		ip[4] = n;
+	} else {
+		ip[4] = 32;
+	};
+
+	return p;
+}
+
+static int gotall(char* p)
+{
+	return (p && !*p);
+}
+
+static void check_parse_ipmask(uint8_t ip[5], char* arg)
+{
+	if(!gotall(parseipmask(arg, ip)))
+		fail("invalid address", arg, 0);
+}
+
+static void check_parse_ipaddr(uint8_t ip[4], char* arg)
+{
+	if(!gotall(parseip(arg, ip)))
+		fail("invalid address", arg, 0);
+}
+
+static void need_one_more_arg(int i, int argc)
+{
+	if(i >= argc)
+		fail("too few arguments", NULL, 0);
+}
+
+static void need_no_more_args(int i, int argc)
+{
+	if(i < argc)
+		fail("too many arguments", NULL, 0);
+}
 
 static void flush_iface(struct netlink* nl, int idx)
 {
--- src/net/utils.h
+++
@@ -1,6 +0,0 @@
-#include <bits/types.h>
-
-void check_parse_ipmask(uint8_t ip[5], char* arg);
-void check_parse_ipaddr(uint8_t ip[4], char* arg);
-void need_one_more_arg(int i, int argc);
-void need_no_more_args(int i, int argc);
