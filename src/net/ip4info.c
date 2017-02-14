f734391e124ae8e115cd8bff8a806a5c35d5a4b7
Alex Suykov <alex.suykov@gmail.com>
2017-02-14 18:09:08 +0200

    ip4info: routes list

--- src/net/ip4info.c
+++ src/net/ip4info.c
@@ -14,8 +14,6 @@
 #include <util.h>
 #include <fail.h>
 
-#include "utils.h"
-
 /* Dumping ip stack state has almost nothing in common with configuring it,
    so this is not a part of ip4cfg, at least for now.
 
@@ -42,9 +40,14 @@ struct ip4addr {
 	uint8_t mask;
 };
 
+struct ip4link {
+	unsigned ifi;
+	char name[20];
+};
+
 char ifbuf[1024];
 
-char txbuf[1024];
+char txbuf[512];
 char rxbuf[6*1024];
 
 static struct nlattr* ifa_get(struct ifaddrmsg* msg, uint16_t key)
@@ -57,7 +60,12 @@ static struct nlattr* ifl_get(struct ifinfomsg* msg, uint16_t key)
 	return nl_attr_k_in(NLPAYLOAD(msg), key);
 }
 
-static char* fmtipm(char* p, char* e, uint8_t* ip, uint8_t mask)
+static struct nlattr* rt_get(struct rtmsg* msg, uint16_t key)
+{
+	return nl_attr_k_in(NLPAYLOAD(msg), key);
+}
+
+static char* fmtip(char* p, char* e, uint8_t* ip)
 {
 	p = fmtint(p, e, ip[0]);
 	p = fmtstr(p, e, ".");
@@ -66,17 +74,50 @@ static char* fmtipm(char* p, char* e, uint8_t* ip, uint8_t mask)
 	p = fmtint(p, e, ip[2]);
 	p = fmtstr(p, e, ".");
 	p = fmtint(p, e, ip[3]);
+	return p;
+}
+
+static char* fmtipm(char* p, char* e, uint8_t* ip, uint8_t mask)
+{
+	p = fmtip(p, e, ip);
 	p = fmtstr(p, e, "/");
 	p = fmtint(p, e, mask);
 	return p;
 }
 
-void note_ip(struct heap* hp, struct ifaddrmsg* msg)
+void** reindex(struct heap* hp, void* p0, void* p1, int size)
 {
-	char* name = nl_str(ifa_get(msg, IFA_LABEL));
+	if((p1 - p0) % size)
+		fail("heap corrupted", NULL, 0);
+
+	int n = (p1 - p0)/size;
+	void** idx = halloc(hp, (n+1)*sizeof(void*));
+
+	int i;
+
+	for(i = 0; i < n; i++)
+		idx[i] = p0 + i*size;
+	idx[i] = NULL;
+
+	return idx;
+}
+
+struct ip4link** index_ifs(struct heap* hp, void* p0, void* p1)
+{
+	return (struct ip4link**)reindex(hp, p0, p1, sizeof(struct ip4link));
+}
+
+struct ip4addr** index_ips(struct heap* hp, void* p0, void* p1)
+{
+	return (struct ip4addr**)reindex(hp, p0, p1, sizeof(struct ip4addr));
+}
+
+void store_ip(struct heap* hp, struct ifaddrmsg* msg)
+{
+	//char* name = nl_str(ifa_get(msg, IFA_LABEL));
 	uint8_t* addr = nl_bin(ifa_get(msg, IFA_ADDRESS), 4);
 
-	if(!name || !addr) return;
+	if(!addr) return;
 
 	struct ip4addr* rec = halloc(hp, sizeof(*rec));
 
@@ -84,23 +125,6 @@ void note_ip(struct heap* hp, struct ifaddrmsg* msg)
 	rec->flags = msg->flags;
 	rec->mask = msg->prefixlen;
 	memcpy(rec->ip, addr, 4);
-}
-
-struct ip4addr** index_ips(struct heap* hp, void* p0, void* p1)
-{
-	struct ip4addr* a0 = p0;
-	struct ip4addr* a1 = p1;
-
-	int n = a1 - a0;
-	struct ip4addr** idx = halloc(hp, (n+1)*sizeof(struct ip4addr*));
-
-	int i;
-
-	for(i = 0; i < n; i++)
-		idx[i] = a0 + i;
-	idx[i] = NULL;
-
-	return idx;
 }
 
 struct ip4addr** fetch_ips(struct netlink* nl, struct heap* hp)
@@ -120,33 +144,73 @@ struct ip4addr** fetch_ips(struct netlink* nl, struct heap* hp)
 	void* p0 = hp->ptr;
 
 	while((nl_recv_multi_into(nl, msg)))
-		note_ip(hp, msg);
+		store_ip(hp, msg);
 	if(nl->err)
 		fail("netlink", "RTM_GETADDR", nl->err);
 
-	void* p1 = hp->end;
+	void* p1 = hp->ptr;
 
 	return index_ips(hp, p0, p1);
 }
 
-void show_iface(struct ifinfomsg* msg, struct ip4addr** ips)
+void store_if(struct heap* hp, struct ifinfomsg* msg)
+{
+	char* name = nl_str(ifl_get(msg, IFLA_IFNAME));
+	struct ip4link* rec = halloc(hp, sizeof(*rec));
+
+	rec->ifi = msg->index;
+	memset(rec->name, 0, sizeof(rec->name));
+
+	if(name && strlen(name) < sizeof(rec->name) - 1)
+		memcpy(rec->name, name, strlen(name));
+}
+
+struct ip4link** fetch_ifs(struct netlink* nl, struct heap* hp)
+{
+	struct ifinfomsg* msg;
+
+	nl_header(nl, msg, RTM_GETLINK, 0,
+		.family = 0,
+		.type = 0,
+		.index = 0,
+		.flags = 0,
+		.change = 0);
+
+	if(nl_send_dump(nl))
+		fail("netlink", "RTM_GETLINK", nl->err);
+
+	void* p0 = hp->ptr;
+
+	while((nl_recv_multi_into(nl, msg)))
+		store_if(hp, msg);
+	if(nl->err)
+		fail("netlink", "RTM_GETLINK", nl->err);
+
+	void* p1 = hp->ptr;
+
+	return index_ifs(hp, p0, p1);
+}
+
+void show_iface(struct ip4link* ifs, struct ip4addr** ips)
 {
 	char* p = ifbuf;
 	char* e = ifbuf + sizeof(ifbuf) - 1;
 
-	char* name = nl_str(ifl_get(msg, IFLA_IFNAME));
+	p = fmtstr(p, e, "link#");
+	p = fmtint(p, e, ifs->ifi);
 
-	if(name)
-		p = fmtstr(p, e, name);
-	else
-		p = fmtint(fmtstr(p, e, "#"), e, msg->index);
+	if(*(ifs->name)) {
+		p = fmtstr(p, e, " ");
+		p = fmtstr(p, e, ifs->name);
+	}
+
 	p = fmtstr(p, e, ":");
 
 	struct ip4addr **qq, *q;
 	int hasaddr = 0;
 
 	for(qq = ips; (q = *qq); qq++) {
-		if(q->ifi != msg->index)
+		if(q->ifi != ifs->ifi)
 			continue;
 		hasaddr = 1;
 		p = fmtstr(p, e, " ");
@@ -159,26 +223,128 @@ void show_iface(struct ifinfomsg* msg, struct ip4addr** ips)
 	writeout(ifbuf, p - ifbuf);
 }
 
-void list_ipconf(struct netlink* nl, struct heap* hp)
+void list_ipconf(struct ip4link** ifs, struct ip4addr** ips)
 {
-	struct ifinfomsg* msg;
+	struct ip4link** q;
 
-	struct ip4addr** ips = fetch_ips(nl, hp);
+	for(q = ifs; *q; q++)
+		show_iface(*q, ips);
+}
 
-	nl_header(nl, msg, RTM_GETLINK, 0,
-		.family = 0,
-		.type = 0,
-		.index = 0,
-		.flags = 0,
-		.change = 0);
+char* fmt_route_dst(char* p, char* e, struct rtmsg* msg)
+{
+	uint8_t* ip = nl_bin(rt_get(msg, RTA_DST), 4);
+
+	if(msg->dst_len == 0) {
+		p = fmtstr(p, e, "default");
+	} else if(!ip) {
+		p = fmtstr(p, e, "no-dst");
+	} else if(msg->dst_len == 32) {
+		p = fmtip(p, e, ip);
+	} else {
+		p = fmtipm(p, e, ip, msg->dst_len);
+	}
+	p = fmtstr(p, e, " ");
+
+	return p;
+}
+
+char* ifi_to_name(struct ip4link** ifs, int ifi)
+{
+	struct ip4link** q;
+
+	for(q = ifs; *q; q++)
+		if((*q)->ifi == ifi)
+			break;
+	
+	return *q ? (*q)->name : NULL;
+}
+
+char* fmt_route_dev(char* p, char* e, struct rtmsg* msg, struct ip4link** ifs)
+{
+	uint32_t* oif;
+	char* name;
+
+	if(!(oif = nl_u32(rt_get(msg, RTA_OIF))))
+		goto out;
+
+	p = fmtstr(p, e, "to ");
+
+	if((name = ifi_to_name(ifs, *oif))) {
+		p = fmtstr(p, e, name);
+	} else {
+		p = fmtstr(p, e, "#");
+		p = fmtint(p, e, *oif);
+	}
+
+	p = fmtstr(p, e, " ");
+out:	
+	return p;
+}
+
+char* fmt_route_gw(char* p, char* e, struct rtmsg* msg)
+{
+	uint8_t* gw = nl_bin(rt_get(msg, RTA_GATEWAY), 4);
+
+	if(gw) {
+		p = fmtstr(p, e, "gw ");
+		p = fmtip(p, e, gw);
+		p = fmtstr(p, e, " ");
+	}
+
+	return p;
+}
+
+char* fmt_route_proto(char* p, char* e, struct rtmsg* msg)
+{
+	int proto = msg->protocol;
+
+	if(proto == RTPROT_STATIC)
+		p = fmtstr(p, e, "static");
+	else if(proto == RTPROT_DHCP)
+		p = fmtstr(p, e, "dhcp");
+	else
+		return p;
+
+	p = fmtstr(p, e, " ");
+	return p;
+}
+
+void show_route(struct rtmsg* msg, struct ip4link** ifs)
+{
+	char* p = ifbuf;
+	char* e = ifbuf + sizeof(ifbuf) - 1;
+
+	if(msg->type == RTN_LOCAL || msg->type == RTN_BROADCAST)
+		return;
+
+	p = fmtstr(p, e, "route ");
+
+	p = fmt_route_dst(p, e, msg);
+	p = fmt_route_dev(p, e, msg, ifs);
+	p = fmt_route_gw(p, e, msg);
+	p = fmt_route_proto(p, e, msg);
+
+	*p++ = '\n';
+	writeout(ifbuf, p - ifbuf);
+}
+
+void list_routes(struct netlink* nl, struct ip4link** ifs)
+{
+	struct rtmsg* msg;
+
+	nl_header(nl, msg, RTM_GETROUTE, 0,
+		.family = AF_INET);
 
 	if(nl_send_dump(nl))
-		fail("netlink", "RTM_GETADDR", nl->err);
+		fail("netlink", "RTM_GETROUTE", nl->err);
+
+	writeout("\n", 1);
 
 	while((nl_recv_multi_into(nl, msg)))
-		show_iface(msg, ips);
+		show_route(msg, ifs);
 	if(nl->err)
-		fail("netlink", "RTM_GETADDR", nl->err);
+		fail("netlink", "RTM_GETROUTE", nl->err);
 }
 
 int main(int argc, char** argv)
@@ -198,7 +364,11 @@ int main(int argc, char** argv)
 	if(i < argc)
 		fail("too many arguments", NULL, 0);
 
-	list_ipconf(&nl, &hp);
+	struct ip4link** ifs = fetch_ifs(&nl, &hp);
+	struct ip4addr** ips = fetch_ips(&nl, &hp);
+
+	list_ipconf(ifs, ips);
+	list_routes(&nl, ifs);
 
 	flushout();
 
