0374650d9c8de023f9173c0e49a3bf0ad1f32d33
Alex Suykov <alex.suykov@gmail.com>
2017-03-02 16:51:01 +0200

    lib: recvfrom()

---
+++ lib/sys/recvfrom.h
@@ -0,0 +1,9 @@
+#include <bits/types.h>
+#include <syscall.h>
+
+inline static long sysrecvfrom(int fd, void* buf, size_t len, int flags,
+		void* addr, int alen)
+{
+	return syscall6(__NR_recvfrom, fd, (long)buf, len, flags,
+			(long)addr, alen);
+}
