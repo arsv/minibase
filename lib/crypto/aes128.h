0f4d7f1cf42b57e72e2f0fa079ac3b967f5d189a
Alex Suykov <alex.suykov@gmail.com>
2017-03-10 17:37:39 +0200

    lib: AES-128 key unwrap

--- lib/crypto/aes128.h
+++ lib/crypto/aes128.h
@@ -7,3 +7,5 @@ struct aes128 {
 void aes128_init(struct aes128* ctx, uint8_t key[16]);
 void aes128_decrypt(struct aes128* ctx, uint8_t blk[16]);
 void aes128_fini(struct aes128* ctx);
+
+void aes128_unwrap(uint8_t key[16], void* buf, int len);
---
+++ lib/crypto/aes128_unwrap.c
@@ -0,0 +1,46 @@
+#include <bits/errno.h>
+#include <string.h>
+#include <endian.h>
+
+#include "aes128.h"
+
+/* Ref. RFC 3394 Advanced Encryption Standard (AES) Key Wrap Algorithm */
+
+static uint64_t wrapmask(int n)
+{
+#ifdef BIGENDIAN
+	return n;
+#else
+	return ((uint64_t)htonl(n) << 32);
+#endif
+}
+
+void aes128_unwrap(uint8_t key[16], void* buf, int len)
+{
+	struct aes128 ae;
+
+	if(len % 8 || len < 16)
+		return;
+
+	int n = len / 8 - 1;
+	uint64_t* R = buf;
+	uint8_t B[16];
+	int i, j;
+
+	aes128_init(&ae, key);
+
+	for(j = 5; j >= 0; j--)
+		for(i = n; i >= 1; i--) {
+			R[0] ^= wrapmask(n*j + i);
+
+			memcpy(B + 0, &R[0], 8);
+			memcpy(B + 8, &R[i], 8);
+
+			aes128_decrypt(&ae, B);
+
+			memcpy(&R[0], B + 0, 8);
+			memcpy(&R[i], B + 8, 8);
+		}
+
+	aes128_fini(&ae);
+}
