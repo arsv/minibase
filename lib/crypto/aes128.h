c9c39b022c17634cfaaba3342ac2bff21eed9b7e
Alex Suykov <alex.suykov@gmail.com>
2017-03-10 17:36:31 +0200

    lib: AES-128 decrypt

---
+++ lib/crypto/aes128.h
@@ -0,0 +1,9 @@
+#include <bits/ints.h>
+
+struct aes128 {
+	uint32_t W[44];  /* 11 round keys, 4x4 each */
+};
+
+void aes128_init(struct aes128* ctx, uint8_t key[16]);
+void aes128_decrypt(struct aes128* ctx, uint8_t blk[16]);
+void aes128_fini(struct aes128* ctx);
