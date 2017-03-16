ab45cf5596a49bd1895645d25f2cd33073831a6a
Alex Suykov <alex.suykov@gmail.com>
2017-03-16 17:26:09 +0200

    wifi: NL family code prep for late subscription
    wpa should only subscribe "scan" group on demand, and will likely
    unsubscribe it once it's done scanning.

--- src/wifi/nlfam.h
+++ src/wifi/nlfam.h
@@ -1,4 +1,9 @@
 struct netlink;
 
+struct nlpair {
+	int id;
+	const char* name;
+};
+
 int query_family(struct netlink* nl, const char* name);
-int query_subscribe(struct netlink* nl, const char** names);
+int query_family_grps(struct netlink* nl, const char* fam, struct nlpair* grps);
