ae473140a340ff0d3ffe779b2303d75fb542f1c1
Alex Suykov <alex.suykov@gmail.com>
2017-03-17 13:41:09 +0200

    wpa: rework NL to handle spontaneous disconnects
    Turns out it's necessary to monitor both EAPOL and NL past
    the initial setup phase. So enter ppoll and other goodies.
    
    Also now it properly disconnects on SIGINT/SIGTERM.

--- src/wifi/wpa.h
+++ src/wifi/wpa.h
@@ -6,21 +6,26 @@ extern int frequency;
 extern uint8_t bssid[6];
 extern char* ssid;
 
+extern int netlink;
+extern int rawsock;
+
 extern uint8_t PSK[32];
 extern uint8_t PTK[16];
 extern uint8_t GTK[32];
 extern uint8_t RSC[6];
 
 void setup_netlink(void);
-void open_netlink(void);
-void close_netlink(void);
 int resolve_ifname(char* name);
 void authenticate(void);
 void associate(void);
 void upload_ptk(void);
 void upload_gtk(void);
+void pull_netlink(void);
+void disconnect(void);
 
 void open_rawsock(void);
 void negotiate_keys(void);
-void group_rekey(void);
+int group_rekey(void);
 void cleanup_keys(void);
+
+void quit(const char* msg, const char* arg, int err);
