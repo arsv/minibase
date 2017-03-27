b45878a0b66d936e6474dc8113ed7ef9d0aafbde
Alex Suykov <alex.suykov@gmail.com>
2017-03-27 16:56:49 +0300

    lib: constant-time memxcmp

---
+++ lib/string/memxcmp.c
@@ -0,0 +1,17 @@
+#include <bits/ints.h>
+#include <string.h>
+
+/* Constant-time memcmp, for crypto stuff */
+
+int memxcmp(const void* av, const void* bv, long len)
+{
+	const uint8_t* a = (const uint8_t*) av;
+	const uint8_t* b = (const uint8_t*) bv;
+	int ret = 0;
+	
+	while(len-- > 0)
+		ret |= (*a++ - *b++);
+
+	return ret;
+}
+
