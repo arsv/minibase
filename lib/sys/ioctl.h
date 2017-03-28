2645d97c6dc2767aafa2f26eda0a42a2575f0ca9
Alex Suykov <alex.suykov@gmail.com>
2017-03-28 23:34:59 +0300

    lib: ioctl canonically takes a pointer

--- lib/sys/ioctl.h
+++ lib/sys/ioctl.h
@@ -1,7 +1,14 @@
 #include <syscall.h>
 #include <bits/ioctl.h>
 
-inline static long sysioctl(int fd, unsigned long request, long arg)
+inline static long sysioctl(int fd, unsigned long request, void* arg)
+{
+	return syscall3(__NR_ioctl, fd, request, (long)arg);
+}
+
+/* same, with immediate/integer argument */
+
+inline static long sysioctli(int fd, unsigned long request, long arg)
 {
 	return syscall3(__NR_ioctl, fd, request, arg);
 }
