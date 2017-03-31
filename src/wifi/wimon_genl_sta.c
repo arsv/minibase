5f6caf795bd9f6d12cc5f31c9491efece821b103
Alex Suykov <alex.suykov@gmail.com>
2017-03-31 03:57:47 +0300

    wimon: parse STA IEs in scan results

--- src/wifi/wimon_genl_sta.c
+++ src/wifi/wimon_genl_sta.c
@@ -20,31 +20,86 @@ struct ies {
 	char payload[];
 };
 
-static const char ms_oui[] = { 0x00, 0x50, 0xf2 };
+/* See also: ies_ccmp, ies_tkip; sadly the structure is somewhat
+   variable, so we can't easily describe it in a struct. */
 
-static void parse_vendor_wpa(struct scan* sc, int len, char* buf)
+static char* get2le(char* p, char* e, uint16_t* v)
 {
-	sc->type |= ST_WPA2_CC; /* XXX */
+	char* q = p; p += 2;
+
+	if(p <= e)
+		*v = (q[0] & 0xFF)
+		  | ((q[1] & 0xFF) << 8);
+
+	return p;
 }
 
-static void parse_vendor_wps(struct scan* sc, int len, char* buf)
+static char* get4be(char* p, char* e, uint32_t* v)
 {
+	char* q = p; p += 4;
+
+	if(p <= e)
+		*v = ((q[0] & 0xFF) << 24)
+		   | ((q[1] & 0xFF) << 16)
+		   | ((q[2] & 0xFF) <<  8)
+		   | ((q[3] & 0xFF));
+
+	return p;
 }
 
-static void parse_vendor_ie(struct scan* sc, int len, char* buf)
+static char* getlist(char* p, char* e, int n, uint32_t** ptr)
 {
-	if(len < 4)
-		return;
-	if(memcmp(buf, ms_oui, sizeof(ms_oui)))
-		return;
+	char* q = p; p += 4*n;
 
-	char* data = buf + 4;
-	int dlen = len - 4;
+	if(p <= e) *ptr = (uint32_t*)q;
 
-	switch(buf[3]) {
-		case 1: return parse_vendor_wpa(sc, dlen, data);
-		case 4: return parse_vendor_wps(sc, dlen, data);
+	return p;
+}
+
+static int inlist(uint32_t* list, int n, int val)
+{
+	int i;
+	uint32_t v;
+
+	for(i = 0; i < n; i++) {
+		char* q = (char*)(&list[i]);
+		v = ((q[0] & 0xFF) << 24)
+		  | ((q[1] & 0xFF) << 16)
+		  | ((q[2] & 0xFF) <<  8)
+		  | ((q[3] & 0xFF));
+		if(v == val) return 1;
 	}
+
+	return 0;
+}
+
+static void parse_rsn_ie(struct scan* sc, int len, char* buf)
+{
+	uint16_t ver, pcnt, acnt;
+	uint32_t group, *pair, *akm;
+
+	char* p = buf;
+	char* e = buf + len;
+
+	p = get2le(p, e, &ver);
+
+	if(ver != 1) return;
+
+	p = get4be(p, e, &group);
+	p = get2le(p, e, &pcnt);
+	p = getlist(p, e, pcnt, &pair);
+	p = get2le(p, e, &acnt);
+	p = getlist(p, e, acnt, &akm);
+
+	if(!inlist(akm, acnt, 0x000FAC02)) /* RSN-PSK */
+		return;
+	if(!inlist(pair, pcnt, 0x000FAC04)) /* CCMP */
+		return;
+
+	if(group == 0x000FAC02)
+		sc->type = ST_WPA2_CT;
+	if(group == 0x000FAC04)
+		sc->type = ST_WPA2_CC;
 }
 
 static void set_station_ssid(struct scan* sc, int len, char* buf)
@@ -71,8 +126,8 @@ void parse_station_ies(struct scan* sc, struct nlattr* at)
 			break;
 		if(ie->type == 0)
 			set_station_ssid(sc, ie->len, ie->payload);
-		else if(ie->type == 221)
-			parse_vendor_ie(sc, ie->len, ie->payload);
+		else if(ie->type == 48)
+			parse_rsn_ie(sc, ie->len, ie->payload);
 
 		ptr += ielen;
 	}
@@ -104,13 +159,21 @@ void parse_scan_result(struct link* ls, struct nlgen* msg)
 	memcpy(sc->bssid, bssid, 6);
 	sc->freq = get_i32_or_zero(bss, NL80211_BSS_FREQUENCY);
 	sc->signal = get_i32_or_zero(bss, NL80211_BSS_SIGNAL_MBM);
+	sc->type = ST_UNKNOWN;
 
 	if((ies = nl_sub(bss, NL80211_BSS_INFORMATION_ELEMENTS)))
 		parse_station_ies(sc, ies);
 
-	eprintf("station %i %i \"%s\"\n", sc->freq, sc->signal/100, sc->ssid);
+	char* type;
 
-	//nl_dump_genl(&msg->nlm);
+	if(sc->type == ST_UNKNOWN)
+		type = "???";
+	else if(sc->type == ST_WPA2_CC)
+		type = "cc";
+	else if(sc->type == ST_WPA2_CT)
+		type = "ct";
+	else
+		type = "?!!";
 
-
+	eprintf("station %i %i \"%s\" %s\n", sc->freq, sc->signal/100, sc->ssid, type);
 }
