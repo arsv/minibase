4550e74844ed79f2a83b51a12198f46d4519a6f3
Alex Suykov <alex.suykov@gmail.com>
2017-04-14 14:02:44 +0300

    msh: rearrange files

--- src/msh/msh_util.c
+++ src/msh/msh_util.c
@@ -1,4 +1,5 @@
 #include <bits/errno.h>
+#include <sys/brk.h>
 #include <sys/_exit.h>
 
 #include <string.h>
@@ -95,4 +96,67 @@ void fatal(struct sh* ctx, const char* err, char* arg)
 {
 	report(ctx->file, ctx->line, err, arg, 0);
 	_exit(0xFF);
+}
+
+/* Heap routines. See msh.h for heap layout. */
+
+#define PAGE 4096
+
+void hinit(struct sh* ctx)
+{
+	void* heap = (void*)sysbrk(0);
+	ctx->heap = heap;
+	ctx->esep = NULL;
+	ctx->csep = heap;
+	ctx->hptr = heap;
+	ctx->hend = (void*)sysbrk(heap + 4096);
+}
+
+void* halloc(struct sh* ctx, int len)
+{
+	void* ret = ctx->hptr;
+
+	if(ctx->hptr + len < ctx->hend)
+		goto ptr;
+
+	int spc = ctx->hend - ctx->hptr - len;
+	spc += (PAGE - spc % PAGE) % PAGE;
+	ctx->hend = (void*)sysbrk(ctx->hend + spc);
+
+	if(ctx->hptr + len < ctx->hend)
+		fail("cannot allocate memory", NULL, 0);
+ptr:
+	ctx->hptr += len;
+
+	return ret;
+}
+
+void hrev(struct sh* ctx, int what)
+{
+	switch(what) {
+		case VSEP:
+			ctx->hptr = ctx->var;
+			ctx->var = NULL;
+			break;
+		case CSEP:
+			ctx->hptr = ctx->csep;
+			break;
+		case ESEP:
+			ctx->csep = NULL;
+			ctx->hptr = ctx->esep;
+			break;
+		case HEAP:
+			ctx->esep = NULL;
+			ctx->csep = NULL;
+			ctx->hptr = ctx->heap;
+	}
+}
+
+void hset(struct sh* ctx, int what)
+{
+	switch(what) {
+		case CSEP: ctx->csep = ctx->hptr; break;
+		case ESEP: ctx->esep = ctx->hptr; break;
+		case VSEP: ctx->var = ctx->hptr; break;
+	}
 }
