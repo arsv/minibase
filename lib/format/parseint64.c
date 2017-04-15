d1eb34556794f459a92ba1a75ce48707fae99cec
Alex Suykov <alex.suykov@gmail.com>
2017-04-15 18:48:29 +0300

    lib: parseu64

---
+++ lib/format/parseint64.c
@@ -0,0 +1,18 @@
+#include <bits/ints.h>
+#include <format.h>
+
+char* parseu64(char* buf, uint64_t* np)
+{
+	uint64_t n = 0;
+	int d;
+	char* p;
+
+	for(p = buf; *p; p++)
+		if(*p >= '0' && (d = *p - '0') < 10)
+			n = n*10 + d;
+		else
+			break;
+
+	*np = n;
+	return p;
+}
