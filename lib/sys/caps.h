9cac216ffc65209b26c0f2e28fad9942cfe79451
Alex Suykov <alex.suykov@gmail.com>
2017-04-20 15:56:31 +0300

    lib: cap-related syscalls

---
+++ lib/sys/caps.h
@@ -0,0 +1,26 @@
+#include <bits/ints.h>
+#include <syscall.h>
+
+#define LINUX_CAPABILITY_VERSION 0x20080522
+#define LINUX_CAPABILITY_U32S    2
+
+struct cap_header {
+	uint32_t version;
+	int pid;
+};
+
+struct cap_data {
+	uint32_t effective;
+	uint32_t permitted;
+	uint32_t inheritable;
+};
+
+inline static long sys_capset(struct cap_header* ch, const struct cap_data* cd)
+{
+	return syscall2(__NR_capset, (long)ch, (long)cd);
+}
+
+inline static long sys_capget(struct cap_header* ch, struct cap_data* cd)
+{
+	return syscall2(__NR_capget, (long)ch, (long)cd);
+}
