146509a4b47c228df7250c7f7076ecb06f1f7040
Alex Suykov <alex.suykov@gmail.com>
2017-04-25 22:25:18 +0300

    hello: comments and minor updates

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,16 @@
 #include <sys/write.h>
 
+/* Since we have the GNU Hello as a fine example of properly designed
+   GNU package, there's got to be this minibase equivalent.
+ 
+   This is how we do it in here. */
+
 int main(void)
 {
-	const char* hello = "Hello, world!\n";
-	syswrite(1, hello, 14);
+	const char msg[] = "Hello, world!\n";
+	int len = sizeof(msg) - 1;
+
+	syswrite(STDOUT, msg, len);
+
 	return 0;
 }
