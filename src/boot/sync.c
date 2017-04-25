6a279b33103118fe0c9114e74050ff239c64d438
Alex Suykov <alex.suykov@gmail.com>
2017-04-25 16:31:59 +0300

    sync: merge fstrim in

--- src/boot/sync.c
+++ src/boot/sync.c
@@ -1,5 +1,8 @@
+#include <bits/ioctl/fstrim.h>
+#include <sys/ioctl.h>
 #include <sys/open.h>
-#include <sys/sync.h>		/* Who could have thought it's a good idea */
+
+#include <sys/sync.h>		/* I wonder who decided it's a good idea   */
 #include <sys/syncfs.h>		/* to have these *four* syscalls instead   */
 #include <sys/fsync.h>		/* of one taking optional fd and flags?    */
 #include <sys/fdatasync.h>
@@ -7,9 +10,10 @@
 #include <util.h>
 #include <fail.h>
 
-#define OPTS "fd"
+#define OPTS "fdt"
 #define OPT_f (1<<0) /* syncfs */
 #define OPT_d (1<<1) /* datasync */
+#define OPT_t (1<<2) /* fstrim */
 
 ERRTAG = "sync";
 ERRLIST = {
@@ -20,6 +24,91 @@ ERRLIST = {
 	REPORT(EWOULDBLOCK), RESTASNUMBERS
 };
 
+static int openref(char* name)
+{
+	const int flags = O_RDONLY | O_NONBLOCK;
+
+	return xchk(sysopen(name, flags), NULL, name);
+}
+
+static void simplesync(int argc, char** argv, int i)
+{
+	if(i >= argc)
+		xchk(syssync(), "sync", NULL);
+	else for(; i < argc; i++)
+		xchk(sysfsync(openref(argv[i])), NULL, argv[i]);
+}
+
+static void fdatasync(int argc, char** argv, int i)
+{
+	if(i >= argc)
+		fail("too few arguments", NULL, 0);
+	for(; i < argc; i++)
+		xchk(sysfdatasync(openref(argv[i])), NULL, argv[i]);
+}
+
+static void syncfs(int argc, char** argv, int i)
+{
+	if(i >= argc)
+		fail("too few arguments", NULL, 0);
+
+	char* name = argv[i++];
+
+	if(i >= argc)
+		fail("too many arguments", NULL, 0);
+
+	int fd = openref(name);
+
+	xchk(syssyncfs(fd), NULL, name);
+}
+
+static void parsesuffixed(uint64_t* u, const char* n)
+{
+	uint64_t tmp = 0;
+	const char* p;
+	int d;
+
+	for(p = n; *p; p++)
+		if(*p >= '0' && (d = *p - '0') <= 9)
+			tmp = tmp*10 + d;
+		else
+			break;
+	switch(*p)
+	{
+		default: fail("invalid number", n, 0);
+		case '\0': break;
+		case 'G': tmp *= 1024;
+		case 'M': tmp *= 1024;
+		case 'K': tmp *= 1024;
+	};
+
+	*u = tmp;
+}
+
+static void fstrim(int argc, char** argv, int i)
+{
+	struct fstrim_range range = { 0, (uint64_t)-1, 0 };
+
+	if(i >= argc)
+		fail("too few arguments", NULL, 0);
+
+	char* name = argv[i++];
+
+	if(i < argc)
+		parsesuffixed(&range.minlen, argv[i++]);
+	if(i < argc)
+		parsesuffixed(&range.start, argv[i++]);
+	if(i < argc)
+		parsesuffixed(&range.len, argv[i++]);
+	if(i < argc)
+		fail("too many arguments", NULL, 0);
+
+	const long flags = O_RDONLY | O_DIRECTORY | O_NONBLOCK;
+	long fd = openref(name);
+
+	xchk(sysioctl(fd, FITRIM, (long)&range), NULL, name);
+}
+
 int main(int argc, char** argv)
 {
 	int i = 1;
@@ -28,26 +117,14 @@ int main(int argc, char** argv)
 	if(i < argc && argv[i][0] == '-')
 		opts = argbits(OPTS, argv[i++] + 1);
 
-	if((opts & (OPT_f | OPT_d)) == (OPT_f | OPT_d))
-		fail("cannot use -f and -d at once", NULL, 0);
-	if((opts & (OPT_f | OPT_d)) && i >= argc)
-		fail("file names must be specified with -f and -d", NULL, 0);
-
-	if(i >= argc) {
-		xchk(syssync(), "sync", NULL);
-	} else for(; i < argc; i++) {
-		const int flags = O_RDONLY | O_NONBLOCK;
-		const char* name = argv[i];
-
-		long fd = xchk(sysopen(name, flags), "open", name);
-
-		if(opts & OPT_d)
-			xchk(sysfdatasync(fd), "fdatasync", name);
-		else if(opts & OPT_f)
-			xchk(syssyncfs(fd), "syncfs", name);
-		else
-			xchk(sysfsync(fd), "fsync", name);
-	}
+	if(!opts)
+		simplesync(argc, argv, i);
+	else if(opts == OPT_t)
+		fstrim(argc, argv, i);
+	else if(opts == OPT_d)
+		fdatasync(argc, argv, i);
+	else if(opts == OPT_f)
+		syncfs(argc, argv, i);
 
 	return 0;
 }
