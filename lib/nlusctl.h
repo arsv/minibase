db118b1e1fd52bb2e9403cd86bddb4f2f094690f
Alex Suykov <alex.suykov@gmail.com>
2017-05-05 12:15:21 +0300

    lib: nlusctl, netlink-based userspace ctl protocol

---
+++ lib/nlusctl.h
@@ -0,0 +1,60 @@
+#include <bits/ints.h>
+
+/* Netlink-based userspace service control protocol.
+
+   Essentially simplified request-reply GENL with hairy stuff like
+   notifications and multipart replies removed.
+
+   This is kept separated from proper netlink code for now, in part
+   because this is only expected to be used in half-duplex mode
+   with a single rx/tx buffer. */
+
+struct ucbuf {
+	char* brk;
+	char* ptr;
+	char* end;
+	int over;
+};
+
+struct ucmsg {
+	uint32_t len;
+	int32_t cmd;
+	char payload[];
+} __attribute__((packed));
+
+struct ucattr {
+	uint16_t len;
+	uint16_t key;
+	char payload[];
+} __attribute__((packed));
+
+void uc_buf_set(struct ucbuf* uc, char* buf, int len);
+
+void uc_put_hdr(struct ucbuf* uc, int cmd);
+void uc_put_end(struct ucbuf* uc);
+
+void uc_put_bin(struct ucbuf* uc, int key, char* buf, int len);
+void uc_put_u16(struct ucbuf* uc, int key, uint16_t v);
+void uc_put_u32(struct ucbuf* uc, int key, uint32_t v);
+void uc_put_str(struct ucbuf* uc, int key, char* str);
+void uc_put_ubin(struct ucbuf* uc, int key, uint8_t* buf, int len);
+
+struct ucattr* uc_put_nest(struct ucbuf* uc, int key);
+void uc_end_nest(struct ucbuf* uc, struct ucattr* nest);
+
+int uc_len(char* buf, int len);
+struct ucmsg* uc_msg(char* buf, int len);
+
+struct ucattr* uc_get_0(struct ucmsg* msg);
+struct ucattr* uc_get_n(struct ucmsg* msg, struct ucattr* at);
+
+struct ucattr* uc_sub_0(struct ucattr* ab);
+struct ucattr* uc_sub_n(struct ucattr* ab, struct ucattr* at);
+
+struct ucattr* uc_get_k(struct ucmsg* msg, int key);
+char* uc_get_bin(struct ucmsg* msg, int key, int len);
+uint32_t* uc_get_u32(struct ucmsg* msg, int key);
+uint16_t* uc_get_u16(struct ucmsg* msg, int key);
+char* uc_get_str(struct ucmsg* msg, int key);
+
+void uc_dump(struct ucmsg* msg);
---
+++ lib/nlusctl/get.c
@@ -0,0 +1,144 @@
+#include <null.h>
+#include "../nlusctl.h"
+
+static struct ucmsg* uc_msg_hdr(char* buf, int len)
+{
+	struct ucmsg* msg = (struct ucmsg*) buf;
+
+	if(len < sizeof(*msg))
+		return NULL;
+
+	return msg;
+}
+
+int uc_len(char* buf, int len)
+{
+	struct ucmsg* msg;
+
+	if(!(msg = uc_msg_hdr(buf, len)))
+		return 0;
+
+	return msg->len;
+}
+
+struct ucmsg* uc_msg(char* buf, int len)
+{
+	struct ucmsg* msg;
+
+	if(!(msg = uc_msg_hdr(buf, len)))
+		return NULL;
+	if(msg->len > len)
+		return NULL;
+
+	return msg;
+}
+
+static struct ucattr* uc_get_0_in(char* buf, int len)
+{
+	struct ucattr* at = (struct ucattr*) buf;
+
+	if(len < sizeof(*at))
+		return NULL;
+	if(at->len > len)
+		return NULL;
+
+	return at;
+}
+
+static struct ucattr* uc_get_n_in(char* buf, int len, struct ucattr* at)
+{
+	char* end = buf + len;
+	char* ptr = (char*) at;
+
+	if(ptr < buf)
+		return NULL;
+	if(ptr > end)
+		return NULL;
+
+	ptr += at->len;
+
+	if(ptr + sizeof(*at) > end)
+		return NULL;
+
+	at = (struct ucattr*) ptr;
+
+	if(ptr + at->len > end)
+		return NULL;
+
+	return at;
+}
+
+struct ucattr* uc_get_0(struct ucmsg* msg)
+{
+	return uc_get_0_in(msg->payload, msg->len - sizeof(*msg));
+}
+
+struct ucattr* uc_get_n(struct ucmsg* msg, struct ucattr* ab)
+{
+	return uc_get_n_in(msg->payload, msg->len - sizeof(*msg), ab);
+}
+
+struct ucattr* uc_sub_0(struct ucattr* ab)
+{
+	return uc_get_0_in(ab->payload, ab->len - sizeof(*ab));
+}
+
+struct ucattr* uc_sub_n(struct ucattr* ab, struct ucattr* at)
+{
+	return uc_get_n_in(ab->payload, ab->len - sizeof(*ab), at);
+}
+
+struct ucattr* uc_get_k(struct ucmsg* msg, int key)
+{
+	struct ucattr* at;
+
+	for(at = uc_get_0(msg); at; at = uc_get_n(msg, at))
+		if(at->key == key)
+			return at;
+
+	return NULL;
+}
+
+char* uc_get_bin(struct ucmsg* msg, int key, int len)
+{
+	struct ucattr* at;
+
+	if(!(at = uc_get_k(msg, key)))
+		return NULL;
+	if(at->len - sizeof(*at) != len)
+		return NULL;
+
+	return at->payload;
+}
+
+uint32_t* uc_get_u32(struct ucmsg* msg, int key)
+{
+	return (uint32_t*)uc_get_bin(msg, key, sizeof(uint32_t));
+}
+
+uint16_t* uc_get_u16(struct ucmsg* msg, int key)
+{
+	return (uint16_t*)uc_get_bin(msg, key, sizeof(uint16_t));
+}
+
+static int is_zstr(char* buf, int len)
+{
+	char* p;
+
+	for(p = buf; p < buf + len - 1; p++)
+		if(!*p) return 0;
+
+	return *p ? 0 : 1;
+}
+
+char* uc_get_str(struct ucmsg* msg, int key)
+{
+	struct ucattr* at;
+
+	if(!(at = uc_get_k(msg, key)))
+		return NULL;
+	if(!is_zstr(at->payload, at->len - sizeof(*at)))
+		return NULL;
+
+	return at->payload;
+}
