eef56e890ce4e2034aa6470a8f825e92482c4394
Alex Suykov <alex.suykov@gmail.com>
2017-06-19 01:11:15 +0300

    wimon: common slot routines

--- src/wifi/wimon_slot.c
+++ src/wifi/wimon_slot.c
@@ -4,6 +4,8 @@
 
 #include "wimon.h"
 
+/* Slot allocation for various objects used in wimon. */
+
 struct link links[NLINKS];
 struct scan scans[NSCANS];
 struct conn conns[NCONNS];
@@ -14,6 +16,44 @@ struct child children[NCHILDREN];
 int nchildren;
 
 struct uplink uplink; /* single one for now */
+
+/* Some time ago this was done in a more human manner similar to find_*
+   functions below, but the code is effectively the same for all kinds
+   of slots, so it has been merged.
+
+   The following assumes an empty slot is always all-bits-0. */
+
+static void* grab_slot(void* slots, int* count, int total, int size)
+{
+	void* ptr = slots + size*(*count);
+	void* end = slots + size*total;
+	void* p;
+
+	for(p = slots; p < ptr; p += size)
+		if(!nonzero(p, size))
+			break;
+	if(p < ptr)
+		return p;
+	if(p >= end)
+		return NULL;
+
+	*count += 1;
+	return p;
+}
+
+static void free_slot(void* slots, int* count, int size, void* p)
+{
+	memzero(p, size);
+
+	while(*count > 0) {
+		p = slots + size*(*count - 1);
+
+		if(nonzero(p, size))
+			break;
+
+		*count -= 1;
+	}
+}
 
 struct link* find_link_slot(int ifi)
 {
@@ -29,23 +69,16 @@ struct link* find_link_slot(int ifi)
 struct link* grab_link_slot(int ifi)
 {
 	struct link* ls;
-	
+
 	if((ls = find_link_slot(ifi)))
 		return ls;
 
-	for(ls = links; ls < links + nlinks; ls++)
-		if(!ls->ifi)
-			return ls;
-	if(ls >= links + NLINKS)
-		return NULL;
-
-	nlinks++;
-	return ls;
+	return grab_slot(links, &nlinks, NLINKS, sizeof(*ls));
 }
 
 void free_link_slot(struct link* ls)
 {
-	memset(ls, 0, sizeof(*ls));
+	free_slot(links, &nlinks, sizeof(*ls), ls);
 }
 
 struct scan* find_scan_slot(uint8_t* bssid)
@@ -68,43 +101,23 @@ struct scan* grab_scan_slot(uint8_t* bssid)
 	if((sc = find_scan_slot(bssid)))
 		return sc;
 
-	for(sc = scans; sc < scans + nscans; sc++)
-		if(!sc->freq)
-			break;
-	if(sc >= scans + NSCANS)
-		return NULL;
-	if(sc == scans + nscans)
-		nscans++;
-
-	return sc;
+	return grab_slot(scans, &nscans, NSCANS, sizeof(*sc));
 }
 
 void free_scan_slot(struct scan* sc)
 {
-	memset(sc, 0, sizeof(*sc));
-
-	if(sc == &scans[nscans-1])
-		nscans--;
+	free_slot(scans, &nscans, sizeof(*sc), sc);
 }
 
 void drop_scan_slots()
 {
+	memzero(scans, nscans*sizeof(*scans));
 	nscans = 0;
 }
 
 struct child* grab_child_slot(void)
 {
-	struct child* ch;
-
-	for(ch = children; ch < children + nchildren; ch++)
-		if(!ch->ifi)
-			break;
-	if(ch >= children + NCHILDREN)
-		return NULL;
-	if(ch >= children + nchildren)
-		nchildren++;
-
-	return ch;
+	return grab_slot(children, &nchildren, NCHILDREN, sizeof(*children));
 }
 
 struct child* find_child_slot(int pid)
@@ -120,28 +133,15 @@ struct child* find_child_slot(int pid)
 
 void free_child_slot(struct child* ch)
 {
-	memzero(ch, sizeof(*ch));
-	if(ch == children + nchildren - 1)
-		nchildren--;
+	free_slot(children, &nchildren, sizeof(*ch), ch);
 }
 
 struct conn* grab_conn_slot(void)
 {
-	struct conn* cn;
-
-	for(cn = conns; cn < conns + nconns; cn++)
-		if(!cn->fd)
-			break;
-	if(cn < conns + nconns)
-		return cn;
-	if(cn >= conns + NCONNS)
-		return NULL;
-
-	nconns++;
-	return cn;
+	return grab_slot(conns, &nconns, NCONNS, sizeof(*conns));
 }
 
 void free_conn_slot(struct conn* cn)
 {
-	memzero(cn, sizeof(*cn));
+	free_slot(conns, &nconns, sizeof(*cn), cn);
 }
