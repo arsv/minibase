e1076268f90f593c8768d60bf9320811caf2beb8
Alex Suykov <alex.suykov@gmail.com>
2017-07-03 01:40:28 +0300

    drop prefix from unused struct rtc_time

--- lib/bits/rtc.h
+++ lib/bits/rtc.h
@@ -3,18 +3,17 @@
 
 /* See also: <linux/rtc.h> */
 
-
 struct rtc_time
 {
-	int tm_sec;
-	int tm_min;
-	int tm_hour;
-	int tm_mday;
-	int tm_mon;
-	int tm_year;
-	int tm_wday;
-	int tm_yday;
-	int tm_isdst;
+	int sec;
+	int min;
+	int hour;
+	int mday;
+	int mon;
+	int year;
+	int wday;
+	int yday;
+	int isdst;
 };
 
 #endif
