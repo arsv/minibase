961cf8aacb9aeb3d1bb4e69af5d3f428bc653007
Alex Suykov <alex.suykov@gmail.com>
2017-07-05 01:48:17 +0300

    lib: setpgid/getpgid
    Unused atm.

--- lib/sys/pgrp.h
+++ lib/sys/pgrp.h
@@ -1,6 +1,21 @@
 #include <syscall.h>
 
+inline static long sys_getsid(void)
+{
+	return syscall0(__NR_getsid);
+}
+
 inline static long sys_setsid(void)
 {
 	return syscall0(__NR_setsid);
+}
+
+inline static long sys_getpgid(int pid)
+{
+	return syscall1(__NR_getpgid, pid);
+}
+
+inline static long sys_setpgid(int pid, int pgid)
+{
+	return syscall2(__NR_setpgid, pid, pgid);
 }
