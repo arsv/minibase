078dbdc0c0dfe637fffb39653097a06cd9813d80
Alex Suykov <alex.suykov@gmail.com>
2017-07-05 17:24:04 +0300

    lib: slower but more readable tm2tv

--- lib/time/tm2tv.c
+++ lib/time/tm2tv.c
@@ -1,32 +1,38 @@
 #include <bits/time.h>
 #include <time.h>
 
-/* This is an adaptation of mktime64 from linux/kernel/time/time.c
-   that works on *tm and *tv.
-
-   struct tm is assumed to be in common userspace format, i.e. year=115
+/* struct tm is assumed to be in common userspace format, i.e. year=115
    for 2015 and mon=0 for January. Days are 1-based. DST is ignored. */
+
+static const short mdays[] = {
+	31, 28, 31, 30,
+	31, 30, 31, 31,
+	30, 31, 30, 31
+};
 
 void tm2tv(struct tm* tm, struct timeval* tv)
 {
-	unsigned int day = tm->mday;
-	unsigned int mon = tm->mon + 1;
-	unsigned int year = tm->year + 1900;
+	int day = tm->mday; /* 1-based */
+	int mon = tm->mon;  /* 0-based */
+	int year = tm->year + 1900;
 
-	/* 1..12 -> 11,12,1..10 */
-	if (0 >= (int) (mon -= 2)) {
-		mon += 12;        /* Puts Feb last since it has leap day */
-		year -= 1;
-	}
+	time_t ts = (year - 1970)*365; /* days */
 
-	time_t ts;
-	
-	ts = (year/4 - year/100 + year/400 + 367*mon/12 + day);
-	ts += year*365 - 719499;  /* days */
+	if(mon < 2) year--; /* do not count possible Feb 29 this year */
 
-	ts = ts*24 + tm->hour;    /* now have hours */
-	ts = ts*60 + tm->min;     /* now have minutes */
-	ts = ts*60 + tm->sec;     /* finally seconds */
+	int leaps_to_1970 = 1970/4 - 1970/100 + 1970/400;
+	int leaps_to_year = year/4 - year/100 + year/400;
+
+	ts += leaps_to_year - leaps_to_1970; /* still days */
+
+	for(int i = 0; i < mon; i++)
+		ts += mdays[i];
+
+	ts += (day - 1); /* account for day being 1-based */
+
+	ts = ts*24 + tm->hour; /* hours */
+	ts = ts*60 + tm->min;  /* minutes */
+	ts = ts*60 + tm->sec;  /* seconds */
 
 	tv->sec = ts;
 	tv->usec = 0;
