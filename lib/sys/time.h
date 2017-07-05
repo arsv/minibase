dcbf97e6040c1b7bda3ac9402dccd272d83b3264
Alex Suykov <alex.suykov@gmail.com>
2017-07-05 21:50:52 +0300

    lib: get/settimeofday style tweaks

--- lib/sys/time.h
+++ lib/sys/time.h
@@ -1,13 +1,14 @@
 #include <syscall.h>
 #include <bits/time.h>
 
-inline static long sys_gettimeofday(struct timeval* tv, struct timezone* tz)
+inline static long sys_gettimeofday(struct timeval* tv,
+                                    struct timezone* tz)
 {
 	return syscall2(__NR_gettimeofday, (long)tv, (long)tz);
 }
 
 inline static long sys_settimeofday(const struct timeval* tv,
-		const struct timezone* tz)
+                                    const struct timezone* tz)
 {
 	return syscall2(__NR_settimeofday, (long)tv, (long)tz);
 }
