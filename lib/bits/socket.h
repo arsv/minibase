af441ec7e461bb3dab588746866dcd4689d2af87
Alex Suykov <alex.suykov@gmail.com>
2017-07-21 10:47:24 +0300

    lib: SEQPACKET socket type

--- lib/bits/socket.h
+++ lib/bits/socket.h
@@ -4,11 +4,14 @@
 #include <bits/types.h>
 #include <bits/socket/unspec.h>
 
-#define SOCK_STREAM    1
-#define SOCK_DGRAM     2
-#define SOCK_RAW       3
-#define SOCK_CLOEXEC   02000000
-#define SOCK_NONBLOCK  04000
+#define SOCK_STREAM     1
+#define SOCK_DGRAM      2
+#define SOCK_RAW        3
+#define SOCK_SEQPACKET  5
+#define SOCK_PACKET    10
+
+#define SOCK_NONBLOCK  (1<<11)
+#define SOCK_CLOEXEC   (1<<19)
 
 #define SOL_SOCKET      1
 #define SO_PEERCRED     17
