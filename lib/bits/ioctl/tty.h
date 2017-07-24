f47a53d41a0c38b87d1e83bed2e42ca33b2e693b
Alex Suykov <alex.suykov@gmail.com>
2017-07-25 00:15:51 +0300

    lib: tty ioctl constants

--- lib/bits/ioctl/tty.h
+++ lib/bits/ioctl/tty.h
@@ -34,3 +34,13 @@ struct vt_stat {
 
 #define KDSETMODE	0x4B3A
 #define KDGETMODE	0x4B3B
+
+#define		K_RAW		0x00
+#define		K_XLATE		0x01
+#define		K_MEDIUMRAW	0x02
+#define		K_UNICODE	0x03
+#define		K_OFF		0x04
+#define KDGKBMODE	0x4B44	/* gets current keyboard mode */
+#define KDSKBMODE	0x4B45	/* sets current keyboard mode */
+
+#define KDSKBMUTE	0x4B51
