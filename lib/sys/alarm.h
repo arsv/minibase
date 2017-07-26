2ddbde00b58a649543e34f2c05c846e80b9eeebe
Alex Suykov <alex.suykov@gmail.com>
2017-07-26 17:54:05 +0300

    lib: replace deperected alarm and sleep syscalls

--- lib/sys/alarm.h
+++ lib/sys/alarm.h
@@ -1,6 +1,12 @@
+#include <bits/time.h>
 #include <syscall.h>
 
 inline static long sys_alarm(unsigned int seconds)
 {
-	return syscall1(__NR_alarm, seconds);
+	struct /* itimerval */ {
+		struct timeval interval;
+		struct timeval value;
+	} itv = { { 0, 0 }, { seconds, 0 } };
+
+	return syscall3(__NR_setitimer, 0 /* ITIMER_REAL */, (long)&itv, 0);
 }
