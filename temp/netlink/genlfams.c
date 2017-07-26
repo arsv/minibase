3809b58e5fb4ea2ac1a7d81e25e42290b7336bef
Alex Suykov <alex.suykov@gmail.com>
2017-07-26 19:21:07 +0300

    temp genlfams tool

---
+++ temp/netlink/genlfams.c
@@ -0,0 +1,102 @@
+#include <bits/errno.h>
+
+#include <netlink.h>
+#include <netlink/genl/ctrl.h>
+#include <netlink/dump.h>
+#include <string.h>
+#include <format.h>
+#include <output.h>
+
+#include <fail.h>
+
+/* The tool queries available GENL families and their multicast groups. */
+
+ERRTAG = "genlfams";
+ERRLIST = {
+	REPORT(ENOENT), REPORT(EINVAL), REPORT(EPERM), REPORT(EFAULT),
+	REPORT(ENOBUFS), RESTASNUMBERS
+};
+
+char rxbuf[4096];
+
+static void dump_group(struct nlattr* mg)
+{
+	char buf[200];
+	char* p = buf;
+	char* e = buf + sizeof(buf) - 1;
+
+	uint32_t* id = nl_sub_u32(mg, 2);
+	char* name = nl_sub_str(mg, 1);
+
+	if(!id || !name)
+		return;
+
+	p = fmtstr(p, e, "    group ");
+	p = fmtint(p, e, *id);
+	p = fmtstr(p, e, ": ");
+	p = fmtstr(p, e, name);
+	*p++ = '\n';
+
+	writeout(buf, p - buf);
+}
+
+static void dump_groups(struct nlgen* msg)
+{
+	struct nlattr* mg;
+	struct nlattr* at;
+
+	if(!(mg = nl_get_nest(msg, CTRL_ATTR_MCAST_GROUPS)))
+		return;
+
+	for(at = nl_sub_0(mg); at; at = nl_sub_n(mg, at))
+		dump_group(at);
+}
+
+
+static void dump_family(struct nlgen* msg)
+{
+	char buf[100];
+	char* p = buf;
+	char* e = buf + sizeof(buf) - 1;
+
+	uint16_t* id = nl_get_u16(msg, CTRL_ATTR_FAMILY_ID);
+	char* name = nl_get_str(msg, CTRL_ATTR_FAMILY_NAME);
+
+	if(!id || !name)
+		return;
+
+	p = fmtstr(p, e, "Family ");
+	p = fmtint(p, e, *id);
+	p = fmtstr(p, e, ": ");
+	p = fmtstr(p, e, name);
+	*p++ = '\n';
+
+	writeout(buf, p - buf);
+
+	dump_groups(msg);
+}
+
+int main(int argc, char** argv)
+{
+	struct netlink nl;
+	struct nlgen* msg;
+	char txbuf[100];
+	int ret;
+
+	nl_init(&nl);
+	nl_set_txbuf(&nl, txbuf, sizeof(txbuf));
+	nl_set_rxbuf(&nl, rxbuf, sizeof(rxbuf));
+	nl_connect(&nl, NETLINK_GENERIC, 0);
+
+	nl_new_cmd(&nl, GENL_ID_CTRL, CTRL_CMD_GETFAMILY, 1);
+
+	if((ret = nl_send_dump(&nl)) < 0)
+		fail("send", NULL, ret);
+
+	while((msg = nl_recv_genl_multi(&nl)))
+		dump_family(msg);
+
+	flushout();
+
+	return 0;
+}
