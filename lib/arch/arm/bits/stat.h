262fbe3703fd3b8002fbc5b8fcf0dc0939d3c4a2
Alex Suykov <alex.suykov@gmail.com>
2017-07-26 16:56:34 +0300

    rework struct stat and related types (x86_64/arm only)

--- lib/arch/arm/bits/stat.h
+++ lib/arch/arm/bits/stat.h
@@ -1,27 +1,27 @@
 #ifndef __BITS_STAT_H__
 #define __BITS_STAT_H__
 
+#include <bits/ints.h>
+#include <bits/time.h>
+
 struct stat {
-	unsigned long long st_dev;	/* Device.  */
-	unsigned long long st_ino;	/* File serial number.  */
-	unsigned int	st_mode;	/* File mode.  */
-	unsigned int	st_nlink;	/* Link count.  */
-	unsigned int	st_uid;		/* User ID of the file's owner.  */
-	unsigned int	st_gid;		/* Group ID of the file's group. */
-	unsigned long long st_rdev;	/* Device number, if device.  */
-	unsigned long long __pad1;
-	long long	st_size;	/* Size of file, in bytes.  */
-	int		st_blksize;	/* Optimal block size for I/O.  */
-	int		__pad2;
-	long long	st_blocks;	/* Number 512-byte blocks allocated. */
-	int		st_atime;	/* Time of last access.  */
-	unsigned int	st_atime_nsec;
-	int		st_mtime;	/* Time of last modification.  */
-	unsigned int	st_mtime_nsec;
-	int		st_ctime;	/* Time of last status change.  */
-	unsigned int	st_ctime_nsec;
-	unsigned int	__unused4;
-	unsigned int	__unused5;
+	uint64_t dev;
+	uint64_t ino;
+	uint32_t mode;
+	uint32_t nlink;
+	uint32_t uid;
+	uint32_t gid;
+	uint64_t rdev;
+	uint64_t __pad1;
+	 int64_t size;
+	 int32_t blksize;
+	 int32_t __pad2;
+	 int64_t blocks;
+	struct timespec atime;
+	struct timespec mtime;
+	struct timespec ctime;
+	uint32_t __unused4;
+	uint32_t __unused5;
 };
 
 #endif
--- lib/bits/types.h
+++ lib/bits/types.h
@@ -8,11 +8,11 @@ typedef unsigned char uint8_t;
 typedef uint64_t ino64_t;
 typedef uint64_t off64_t;
 
-typedef unsigned long size_t;
-typedef long time_t;
-
 typedef int uid_t;
 typedef int gid_t;
 typedef int pid_t;
+
+typedef unsigned long size_t;
+typedef uint64_t off_t;
 
 #endif
