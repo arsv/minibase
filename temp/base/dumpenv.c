105756f63f8f0de13073190b3320ae48981b3f35
Alex Suykov <alex.suykov@gmail.com>
2017-07-26 17:11:01 +0300

    temp: syscall updates

--- temp/base/dumpenv.c
+++ temp/base/dumpenv.c
@@ -1,11 +1,11 @@
-#include <format.h>
+#include <printf.h>
 
 int main(int argc, char** argv, char** envp)
 {
 	char** p;
 
 	for(p = envp; *p; p++)
-		eprintf("[%i] %s\n", p - envp, *p);
+		tracef("[%i] %s\n", p - envp, *p);
 
 	return 0;
 }
