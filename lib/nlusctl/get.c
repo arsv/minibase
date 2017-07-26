d1e6bfa08ce196271ff836f208f2433c25479e14
Alex Suykov <alex.suykov@gmail.com>
2017-07-26 03:39:26 +0300

    lib NLUS fixes

--- lib/nlusctl/get.c
+++ lib/nlusctl/get.c
@@ -114,15 +114,6 @@ struct ucattr* uc_sub(struct ucattr* bt, int key)
 	return NULL;
 }
 
-static int is_zstr(char* buf, int len)
-{
-	char* p;
-
-	for(p = buf; p < buf + len - 1; p++)
-		if(!*p) return 0;
-
-	return *p ? 0 : 1;
-}
 void* uc_payload(struct ucattr* at)
 {
 	return (void*)(at->payload);
@@ -131,59 +122,4 @@ void* uc_payload(struct ucattr* at)
 int uc_paylen(struct ucattr* at)
 {
 	return at->len - sizeof(*at);
-}
-
-void* uc_is_bin(struct ucattr* at, int key, int len)
-{
-	if(!at || at->key != key)
-		return NULL;
-	if(uc_paylen(at) != len)
-		return NULL;
-
-	return uc_payload(at);
-}
-
-char* uc_is_str(struct ucattr* at, int key)
-{
-	if(!at || at->key != key)
-		return NULL;
-	if(!is_zstr(uc_payload(at), uc_paylen(at)))
-		return NULL;
-
-	return uc_payload(at);
-}
-
-int* uc_is_int(struct ucattr* at, int key)
-{
-	return uc_is_bin(at, key, 4);
-}
-
-void* uc_get_bin(struct ucmsg* msg, int key, int len)
-{
-	return uc_is_bin(uc_get(msg, key), key, len);
-}
-
-int* uc_get_int(struct ucmsg* msg, int key)
-{
-	return uc_is_int(uc_get(msg, key), key);
-}
-
-char* uc_get_str(struct ucmsg* msg, int key)
-{
-	return uc_is_str(uc_get(msg, key), key);
-}
-
-void* uc_sub_bin(struct ucattr* at, int key, int len)
-{
-	return uc_is_bin(uc_sub(at, key), key, len);
-}
-
-int* uc_sub_int(struct ucattr* at, int key)
-{
-	return uc_is_int(uc_sub(at, key), key);
-}
-
-char* uc_sub_str(struct ucattr* at, int key)
-{
-	return uc_is_str(uc_sub(at, key), key);
 }
