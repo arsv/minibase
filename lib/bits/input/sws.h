8a74311c88ffef1d753a71fdff13bce4d161645b
Alex Suykov <alex.suykov@gmail.com>
2017-08-02 14:00:52 +0300

    lib: input switch bits

---
+++ lib/bits/input/sws.h
@@ -0,0 +1,21 @@
+#ifndef __BITS_INPUT_SWS_H__
+#define __BITS_INPUT_SWS_H__
+
+#define SW_LID			0x00
+#define SW_TABLET_MODE		0x01
+#define SW_HEADPHONE_INSERT	0x02
+#define SW_RFKILL_ALL		0x03
+#define SW_MICROPHONE_INSERT	0x04
+#define SW_DOCK			0x05
+#define SW_LINEOUT_INSERT	0x06
+#define SW_JACK_PHYSICAL_INSERT 0x07
+#define SW_VIDEOOUT_INSERT	0x08
+#define SW_CAMERA_LENS_COVER	0x09
+#define SW_KEYPAD_SLIDE		0x0a
+#define SW_FRONT_PROXIMITY	0x0b
+#define SW_ROTATE_LOCK		0x0c
+#define SW_LINEIN_INSERT	0x0d
+#define SW_MUTE_DEVICE		0x0e
+#define SW_PEN_INSERTED		0x0f
+
+#endif
