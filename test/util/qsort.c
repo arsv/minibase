7ffe47dffc6735984be47bb017c5b281ffdd9b52
Alex Suykov <alex.suykov@gmail.com>
2017-08-04 16:34:26 +0300

    test qsort - more cases and simpler code

--- test/util/qsort.c
+++ test/util/qsort.c
@@ -1,10 +1,21 @@
 #include <util.h>
 #include <format.h>
 
-static int intcmp(const void* a, const void* b, long _)
+static long cast(const void* ptr, long sz)
 {
-	int va = *((int*)a);
-	int vb = *((int*)b);
+	if(sz == sizeof(long))
+		return *((long*)ptr);
+	if(sz == sizeof(int))
+		return *((int*)ptr);
+	if(sz == sizeof(char))
+		return *((char*)ptr);
+	return 0;
+}
+
+static int cmp(const void* a, const void* b, long sz)
+{
+	long va = cast(a, sz);
+	long vb = cast(b, sz);
 
 	if(va < vb)
 		return -1;
@@ -14,24 +25,17 @@ static int intcmp(const void* a, const void* b, long _)
 	return 0;
 }
 
-static char* format_tag(char* p, char* e, const char* file, int line)
-{
-	p = fmtstr(p, e, file);
-	p = fmtstr(p, e, ":");
-	p = fmtint(p, e, line);
-	p = fmtstr(p, e, ": ");
-	return p;
-}
-
-static char* put_array(char* p, char* e, int* a, int n)
+static char* put_array(char* p, char* e, void* a, size_t n, size_t sz)
 {
 	int i;
+	void* v = a;
 
 	p = fmtstr(p, e, " { ");
 
 	for(i = 0; i < n; i++) {
 		p = fmtstr(p, e, i ? ", " : "");
-		p = fmtint(p, e, a[i]);
+		p = fmtlong(p, e, cast(v, sz));
+		v += sz;
 	}
 
 	p = fmtstr(p, e, " }");
@@ -39,112 +43,79 @@ static char* put_array(char* p, char* e, int* a, int n)
 	return p;
 }
 
-static void end_format(char* s, char* p)
+static int check_order(void* a, size_t n, size_t sz)
 {
-	*p++ = '\n';
-	writeall(STDERR, s, p - s);
-}
+	size_t i;
+	void *u, *v = a;
 
-static int error(char* s, char* p, char* e, char* msg)
-{
-	p = fmtstr(p, e, "ERROR ");
-	p = fmtstr(p, e, msg);
-	end_format(s, p);
-
-	return -1;
-}
-
-static int failure(char* s, char* p, char* e, int* a, int* b, int n)
-{
-	char* q = p;
-
-	p = fmtstr(p, e, "FAIL");
-	p = put_array(p, e, a, n);
-	end_format(s, p);
-
-	p = fmtstr(q, e, "    ");
-	p = put_array(p, e, b, n);
-	end_format(s, p);
-
-	return 1;
-}
-
-static int success(char* s, char* p, char* e, int* a, int n)
-{
-	p = fmtstr(p, e, "OK");
-	p = put_array(p, e, a, n);
-	end_format(s, p);
-
-	return 0;
-}
-
-static int compare(int* a, int* b, int n)
-{
-	int i;
-
-	for(i = 0; i < n; i++)
-		if(a[i] != b[i])
+	for(i = 1; i < n; i++)
+		if(cmp(v, (u = v + sz), sz) > 0)
 			return -1;
+		else
+			v = u;
 
 	return 0;
 }
 
-static int test(char* file, int line, int* a, int na, int* b, int nb)
+static int test(char* file, int line, void* a, size_t n, size_t sz)
 {
 	char buf[200];
-	char* s = buf;
 	char* p = buf;
 	char* e = buf + sizeof(buf) - 1;
+	int ret;
 
-	p = format_tag(p, e, file, line);
+	p = fmtstr(p, e, file);
+	p = fmtstr(p, e, ":");
+	p = fmtint(p, e, line);
+	p = fmtstr(p, e, ": ");
 
-	if(na != nb)
-		return error(s, p, e, "inconsistent array sizes");
+	qsort(a, n, sz, cmp, sz);
 
-	qsort(a, na, sizeof(*a), intcmp, 0);
+	if((ret = check_order(a, n, sz)))
+		p = fmtstr(p, e, "FAIL");
+	else
+		p = fmtstr(p, e, "OK");
 
-	if(compare(a, b, na))
-		return failure(s, p, e, a, b, na);
+	p = put_array(p, e, a, n, sz);
 
-	return success(s, p, e, a, na);
+	*p++ = '\n';
+	writeall(STDOUT, buf, p - buf);
+
+	return ret;
 }
 
-#define q(...) { __VA_ARGS__ }
-
-#define TEST(a, s) \
+#define TEST(type, ...) \
 {\
-	int A[] = a; \
-	int S[] = s; \
-	ret |= test(__FILE__, __LINE__, A, ARRAY_SIZE(A), S, ARRAY_SIZE(S)); \
+	type A[] = { __VA_ARGS__ }; \
+	ret |= test(__FILE__, __LINE__, A, ARRAY_SIZE(A), sizeof(*A)); \
 }
 
 int main(void)
 {
 	int ret = 0;
 
-	TEST(q(), q());
+	TEST(int);
+	TEST(int, 1, 0);
+	TEST(int, 2, 1, 0);
+	TEST(int, 2, 3, 1, 0);
 
-	TEST(q(1, 0), q(0, 1));
+	TEST(int, 8, 4, 8, 1, 4, 1, 4, 1, 8);
+	TEST(long, 8, 4, 8, 1, 4, 1, 4, 1, 8);
 
-	TEST(q(2, 1, 0), q(0, 1, 2));
+	TEST(int, 1, 6, 5, 3, 4, 2, 7, 0);
+	TEST(int, 0, 1, 2, 3, 4, 5, 6, 7, 8);
+	TEST(int, 8, 7, 6, 5, 4, 3, 2, 1, 0);
 
-	TEST(q( 1, 6, 5, 3, 4, 2, 7, 0 ),
-	     q( 0, 1, 2, 3, 4, 5, 6, 7 ));
+	TEST(char, 0, 1, 2, 3, 3, 3, 4, 5, 6);
 
-	TEST(q( 0, 1, 2, 3, 4, 5, 6, 7, 8 ),
-	     q( 0, 1, 2, 3, 4, 5, 6, 7, 8 ));
+	TEST(int, 3, 1, 3, 0, 6, 2, 4, 5, 3);
+	TEST(int, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+	TEST(int, 1, 2, 2, 1, 1, 1, 1, 1, 1);
+	TEST(int, 1, 2, 2, 1, 3, 1, 1, 1, 1);
+	TEST(int, 1, 2, 2, 1, 0, 1, 1, 1, 1);
 
-	TEST(q( 8, 7, 6, 5, 4, 3, 2, 1, 0 ),
-	     q( 0, 1, 2, 3, 4, 5, 6, 7, 8 ));
-
-	TEST(q( 0, 1, 2, 3, 3, 3, 4, 5, 6 ),
-	     q( 0, 1, 2, 3, 3, 3, 4, 5, 6 ));
-
-	TEST(q( 3, 1, 3, 0, 6, 2, 4, 5, 3 ),
-	     q( 0, 1, 2, 3, 3, 3, 4, 5, 6 ));
-
-	TEST(q( 1, 1, 1, 1, 1, 1, 1, 1, 1 ),
-	     q( 1, 1, 1, 1, 1, 1, 1, 1, 1 ));
+	TEST(int, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11,
+	          10,  9,  8,  7,  6,  5,  4,  3,  2,  1);
 
 	return ret;
 }
