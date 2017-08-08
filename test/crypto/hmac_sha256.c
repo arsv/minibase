88eaa3c886e3fa7f580fa2b35dd435b4c71dd43d
Alex Suykov <alex.suykov@gmail.com>
2017-08-08 14:55:00 +0300

    test sha256

---
+++ test/crypto/hmac_sha256.c
@@ -0,0 +1,82 @@
+#include <crypto/sha256.h>
+#include <string.h>
+#include <printf.h>
+
+/* Tests from RFC 4868 */
+
+struct test {
+	int inlen;
+	char* input;
+	int keylen;
+	uint8_t key[64];
+	uint8_t hash[32];
+} tests[] = {
+	/* Test case 1 */
+	{ 8, "Hi There", 32,
+	      { 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b,
+	        0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b,
+	        0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b,
+	        0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b },
+	      { 0x19, 0x8a, 0x60, 0x7e, 0xb4, 0x4b, 0xfb, 0xc6,
+	        0x99, 0x03, 0xa0, 0xf1, 0xcf, 0x2b, 0xbd, 0xc5,
+	        0xba, 0x0a, 0xa3, 0xf3, 0xd9, 0xae, 0x3c, 0x1c,
+	        0x7a, 0x3b, 0x16, 0x96, 0xa0, 0xb6, 0x8c, 0xf7,
+	      } },
+	{ 0, NULL, 0, { 0 }, { 0 } }
+};
+
+void dump(uint8_t hash[20])
+{
+	for(int i = 0; i < 20; i++)
+		tracef("%02X ", hash[i]);
+	tracef("\n");
+}
+
+int printable(char* msg)
+{
+	char* p;
+
+	for(p = msg; *p; p++)
+		if(*p & 0x80 || *p < 0x20)
+			return 0;
+
+	return 1;
+}
+
+int test(struct test* tp)
+{
+	int inlen = tp->inlen;
+	char* input = tp->input;
+	int klen = tp->keylen;
+	uint8_t* key = tp->key;
+	uint8_t* hash = tp->hash;
+	uint8_t temp[32];
+
+	hmac_sha256(temp, key, klen, input, inlen);
+
+	int diff = memcmp(hash, temp, 20);
+
+	char* msg = tp->input;
+
+	if(!diff) {
+		tracef("OK %s\n", msg);
+	} else {
+		tracef("FAIL %s\n", msg);
+
+		dump(hash);
+		dump(temp);
+	}
+
+	return diff;
+}
+
+int main(void)
+{
+	struct test* tp;
+	int failure = 0;
+
+	for(tp = tests; tp->input; tp++)
+		failure |= test(tp);
+
+	return failure ? -1 : 0;
+}
---
+++ test/crypto/sha256.c
@@ -0,0 +1,81 @@
+#include <crypto/sha256.h>
+#include <string.h>
+#include <printf.h>
+
+void dump(char* tag, uint8_t hash[32])
+{
+	printf(" %s: ", tag);
+	for(int i = 0; i < 32; i++)
+		printf("%02X ", hash[i]);
+	printf("\n");
+}
+
+int printable(char* msg)
+{
+	char* p;
+
+	for(p = msg; *p; p++)
+		if(*p & 0x80 || *p < 0x20)
+			return 0;
+
+	return 1;
+}
+
+int test(char* msg, uint8_t hash[32])
+{
+	uint8_t temp[32];
+
+	sha256(temp, msg, strlen(msg));
+
+	int diff = memcmp(hash, temp, 20);
+
+	if(!printable(msg))
+		msg = "<non-printable>";
+
+	if(!diff) {
+		printf("OK %s\n", msg);
+	} else {
+		printf("FAIL %s\n", msg);
+
+		dump("exp", hash);
+		dump("got", temp);
+	}
+
+	return diff;
+}
+
+struct test {
+	char* input;
+	uint8_t hash[32];
+} tests[] = {
+	/* Empty input */
+	{ "", {
+		0XE3,0XB0,0XC4,0X42,0X98,0XFC,0X1C,0X14,
+		0X9A,0XFB,0XF4,0XC8,0X99,0X6F,0XB9,0X24,
+		0X27,0XAE,0X41,0XE4,0X64,0X9B,0X93,0X4C,
+		0XA4,0X95,0X99,0X1B,0X78,0X52,0XB8,0X55 } },
+	/* RFC TEST1 */
+	{ "abc", {
+		0xBA,0x78,0x16,0xBF,0x8F,0x01,0xCF,0xEA,
+		0x41,0x41,0x40,0xDE,0x5D,0xAE,0x22,0x23,
+		0xB0,0x03,0x61,0xA3,0x96,0x17,0x7A,0x9C,
+		0xB4,0x10,0xFF,0x61,0xF2,0x00,0x15,0xAD } },
+	/* RFC TEST2_1 */
+	{ "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", {
+		0x24,0x8D,0x6A,0x61,0xD2,0x06,0x38,0xB8,
+		0xE5,0xC0,0x26,0x93,0x0C,0x3E,0x60,0x39,
+		0xA3,0x3C,0xE4,0x59,0x64,0xFF,0x21,0x67,
+		0xF6,0xEC,0xED,0xD4,0x19,0xDB,0x06,0xC1 } },
+	{ NULL, { 0 } }
+};
+
+int main(void)
+{
+	struct test* tp;
+	int failure = 0;
+
+	for(tp = tests; tp->input; tp++)
+		failure |= test(tp->input, tp->hash);
+
+	return failure ? -1 : 0;
+}
