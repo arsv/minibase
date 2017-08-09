79a6e99ae9ea95cfb2a0312813bf3338ebc8a610
Alex Suykov <alex.suykov@gmail.com>
2017-08-09 13:07:45 +0300

    lib: tweak scrypt copyright notice
    This particular file has never been written for the Tarspan system,
    that's just wrong.

--- lib/crypto/scrypt.c
+++ lib/crypto/scrypt.c
@@ -1,4 +1,4 @@
-/* Colin Percival's code, slightly modified to fit the minibase style.
+/* Colin Percival's code, modified to fit the minibase style.
    In this version the caller is responsible for allocating memory.
    Original copyright notice follows: */
 /*-
@@ -8,8 +8,10 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -24,11 +26,7 @@
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file was originally written by Colin Percival as part of the Tarsnap
- * online backup system.
- */
+ * SUCH DAMAGE. */
 
 #include <bits/ints.h>
 #include <bits/errno.h>
