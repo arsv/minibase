e45c44e516307dacc4729c470ec870b75d9e1807
Alex Suykov <alex.suykov@gmail.com>
2017-08-17 10:19:04 +0300

    findblk: move linking code into findblk.c

--- src/dcrypt/findblk.c
+++ src/dcrypt/findblk.c
@@ -1,3 +1,8 @@
+#include <sys/file.h>
+#include <sys/fsnod.h>
+#include <sys/symlink.h>
+
+#include <format.h>
 #include <fail.h>
 
 #include "config.h"
@@ -10,6 +15,32 @@ struct part parts[NPARTS];
 
 int nbdevs;
 int nparts;
+
+static void link_part(char* name, char* label)
+{
+	FMTBUF(lp, le, link, 100);
+	lp = fmtstr(lp, le, MAPDIR);
+	lp = fmtstr(lp, le, "/");
+	lp = fmtstr(lp, le, label);
+	FMTEND(lp);
+
+	FMTBUF(pp, pe, path, 100);
+	pp = fmtstr(pp, pe, "/dev/");
+	pp = fmtstr(pp, pe, name);
+	FMTEND(pp);
+
+	sys_symlink(path, link);
+}
+
+void link_parts(void)
+{
+	struct part* pt;
+
+	sys_mkdir(MAPDIR, 0755);
+
+	for(pt = parts; pt < parts + nparts; pt++)
+		link_part(pt->name, pt->label);
+}
 
 int main(int argc, char** argv)
 {
