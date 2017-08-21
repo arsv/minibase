5404f6b1f620b126580893009f623d9057e3805d
Alex Suykov <alex.suykov@gmail.com>
2017-08-21 14:49:40 +0300

    rules.mk for temp/ directory

---
+++ temp/rules.mk
@@ -0,0 +1,33 @@
+.SUFFIXES:
+.SECONDARY:
+
+/ := $(dir $(wildcard ../configure ../../configure ../../../configure))
+
+include $/config.mk
+
+%.o: %.c
+	$(CC)$(if $(CFLAGS), $(CFLAGS)) -c $<
+
+%: %.o
+	$(LD) -o $@ $(filter %.o,$^) $(LIBS)
+
+all: $(bin) $(sbin) $(also)
+
+clean:
+	rm -f *.o $(bin) $(sbin) $(also)
+
+$(DESTDIR)/bin $(DESTDIR)/sbin:
+	mkdir -p $@
+
+install = $(foreach _, bin sbin man1 man8, $(if $($_),install-$_))
+install: $(install)
+install-man: $(filter install-man%,$(install))
+
+install-bin install-sbin: install-%:
+	  mkdir -p $(DESTDIR)$($*dir)
+	  cp -a $($*) $(DESTDIR)$($*dir)
+	  $(STRIP) $(patsubst %,$(DESTDIR)$($*dir)/%,$($*))
+
+install-man%:
+	mkdir -p $(DESTDIR)$(man$*dir)
+	cp $(man$*) $(DESTDIR)$(man$*dir)
