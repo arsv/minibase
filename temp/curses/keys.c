311165fb66dc14c750486243a83b6d0ae53904e7
Alex Suykov <alex.suykov@gmail.com>
2017-08-21 15:09:06 +0300

    temp curses stuff

---
+++ temp/curses/keys.c
@@ -0,0 +1,165 @@
+#include <bits/ioctl/tty.h>
+#include <sys/ioctl.h>
+#include <sys/file.h>
+
+#include <printf.h>
+#include <format.h>
+#include <string.h>
+#include <fail.h>
+#include <util.h>
+
+#define CSI "\033["
+
+int rows;
+int cols;
+
+static void output(char* s, int len)
+{
+	sys_write(STDOUT, s, len);
+}
+
+static void outstr(char* s)
+{
+	output(s, strlen(s));
+}
+
+static void moveto(int r, int c)
+{
+	char buf[20];
+	char* p = buf;
+	char* e = buf + sizeof(buf) - 1;
+
+	p = fmtstr(p, e, CSI);
+	p = fmtint(p, e, r);
+	p = fmtstr(p, e, ";");
+	p = fmtint(p, e, c);
+	p = fmtstr(p, e, "H");
+
+	output(buf, p - buf);
+}
+
+static void makeline(char* l, char* m, char* r, int n)
+{
+	int i;
+
+	outstr(l);
+	for(i = 1; i < n - 1; i++)
+		outstr(m);
+	outstr(r);
+}
+
+static void message(char* msg)
+{
+	int len = strlen(msg);
+
+	int mw = (len < 20 ? 20 : len);
+	int ww = mw + 6;
+	int mc = cols/2 - len/2;
+
+	int wc = cols/2 - mw/2 - 3;
+	int wr = rows/2 - 2;
+
+	moveto(wr, wc);
+	makeline("┌", "─", "┐", ww);
+	moveto(wr + 1, wc);
+	makeline("│", " ", "│", ww);
+	moveto(wr + 2, wc);
+	makeline("│", " ", "│", ww);
+	moveto(wr + 3, wc);
+	makeline("│", " ", "│", ww);
+	moveto(wr + 4, wc);
+	makeline("└", "─", "┘", ww);
+
+	moveto(wr + 2, mc);
+	outstr(msg);
+}
+
+static void clear(void)
+{
+	moveto(1,1);
+	outstr(CSI "J");
+}
+
+static void prep_cursor(void)
+{
+	int wr = rows/2 + 5;
+	int wc = cols/2 - 10;
+	moveto(wr, wc);
+}
+
+static void show_input(void* buf, int len)
+{
+	unsigned char* v = buf;
+	char out[10];
+	int wr = rows/2 + 5;
+	int wc = cols/2 - 10;
+	int i;
+
+	moveto(wr, wc);
+	outstr(CSI "K");
+
+	for(i = 0; i < len; i++) {
+		char* p = out;
+		char* e = out + sizeof(out) - 1;
+
+		p = fmtint(p, e, v[i]);
+		p = fmtstr(p, e, " ");
+
+		output(out, p - out);
+	};
+}
+
+static int got_ctrl_d(char* buf, int len)
+{
+	char* p = buf;
+	char* e = buf + len;
+
+	for(; p < e; p++)
+		if(*p == 4)
+			return 1;
+
+	return 0;
+}
+
+int main(void)
+{
+	struct winsize ws;
+	struct termios ts, tso;
+	int ret;
+	char buf[100];
+
+	if((ret = sys_ioctl(0, TIOCGWINSZ, &ws)) < 0)
+		fail("ioctl", "TIOCGWINSZ", ret);
+
+	rows = ws.row;
+	cols = ws.col;
+
+	if((ret = sys_ioctl(0, TCGETS, &ts)) < 0)
+		fail("ioctl", "TCGETS", ret);
+
+	memcpy(&tso, &ts, sizeof(ts));
+	ts.iflag &= ~(IGNPAR | ICRNL | IXON | IMAXBEL);
+	ts.oflag &= ~(OPOST | ONLCR);
+	ts.lflag &= ~(ICANON | ECHO);
+
+	if((ret = sys_ioctl(0, TCSETS, &ts)) < 0)
+		fail("ioctl", "TCSETS", ret);
+
+	clear();
+	message("Tap keys to see their event codes. Press C-d when done.");
+	prep_cursor();
+
+	while((ret = sys_read(STDIN, buf, sizeof(buf))) > 0) {
+		show_input(buf, ret);
+
+		if(got_ctrl_d(buf, ret))
+			break;
+	}
+
+	moveto(ws.row, 1);
+
+	if((ret = sys_ioctl(0, TCSETS, &tso)) < 0)
+		fail("ioctl", "TCSETS", ret);
+
+	return 0;
+}
---
+++ temp/curses/tcattr.c
@@ -0,0 +1,137 @@
+#include <bits/ioctl/tty.h>
+#include <sys/ioctl.h>
+
+#include <printf.h>
+#include <fail.h>
+#include <util.h>
+
+struct dict {
+	char* name;
+	int bits;
+	int mask;
+} iflags[] = {
+	{ "IGNBRK",   0000001, 0000001 },
+	{ "BRKINT",   0000002, 0000002 },
+	{ "IGNPAR",   0000004, 0000004 },
+	{ "PARMRK",   0000010, 0000010 },
+	{ "INPCK",    0000020, 0000020 },
+	{ "ISTRIP",   0000040, 0000040 },
+	{ "INLCR",    0000100, 0000100 },
+	{ "IGNCR",    0000200, 0000200 },
+	{ "ICRNL",    0000400, 0000400 },
+	{ "IUCLC",    0001000, 0001000 },
+	{ "IXON",     0002000, 0002000 },
+	{ "IXANY",    0004000, 0004000 },
+	{ "IXOFF",    0010000, 0010000 },
+	{ "IMAXBEL",  0020000, 0020000 },
+	{ "IUTF8",    0040000, 0040000 },
+}, oflags[] = {
+	{ "OPOST",    0000001, 0000001 },
+	{ "OLCUC",    0000002, 0000002 },
+	{ "ONLCR",    0000004, 0000004 },
+	{ "OCRNL",    0000010, 0000010 },
+	{ "ONOCR",    0000020, 0000020 },
+	{ "ONLRET",   0000040, 0000040 },
+	{ "OFILL",    0000100, 0000100 },
+	{ "OFDEL",    0000200, 0000200 },
+}, cflags[] = {
+	{ "B0",       0000000, 0010017 },
+	{ "B50",      0000001, 0010017 },
+	{ "B75",      0000002, 0010017 },
+	{ "B110",     0000003, 0010017 },
+	{ "B134",     0000004, 0010017 },
+	{ "B150",     0000005, 0010017 },
+	{ "B200",     0000006, 0010017 },
+	{ "B300",     0000007, 0010017 },
+	{ "B600",     0000010, 0010017 },
+	{ "B1200",    0000011, 0010017 },
+	{ "B1800",    0000012, 0010017 },
+	{ "B2400",    0000013, 0010017 },
+	{ "B4800",    0000014, 0010017 },
+	{ "B9600",    0000015, 0010017 },
+	{ "B19200",   0000016, 0010017 },
+	{ "B38400",   0000017, 0010017 },
+	{ "CS5",      0000000, 0000060 },
+	{ "CS6",      0000020, 0000060 },
+	{ "CS7",      0000040, 0000060 },
+	{ "CS8",      0000060, 0000060 },
+	{ "CSTOPB",   0000100, 0000100 },
+	{ "CREAD",    0000200, 0000200 },
+	{ "PARENB",   0000400, 0000400 },
+	{ "PARODD",   0001000, 0001000 },
+	{ "HUPCL",    0002000, 0002000 },
+	{ "CLOCAL",   0004000, 0004000 },
+	{ "BOTHER",   0010000, 0010000 },
+}, lflags[] = {
+	{ "ISIG",     0000001, 0000001 },
+	{ "ICANON",   0000002, 0000002 },
+	{ "XCASE",    0000004, 0000004 },
+	{ "ECHO",     0000010, 0000010 },
+	{ "ECHOE",    0000020, 0000020 },
+	{ "ECHOK",    0000040, 0000040 },
+	{ "ECHONL",   0000100, 0000100 },
+	{ "NOFLSH",   0000200, 0000200 },
+	{ "TOSTOP",   0000400, 0000400 },
+	{ "ECHOCTL",  0001000, 0001000 },
+	{ "ECHOPRT",  0002000, 0002000 },
+	{ "ECHOKE",   0004000, 0004000 },
+	{ "FLUSHO",   0010000, 0010000 },
+	{ "PENDIN",   0040000, 0040000 },
+	{ "IEXTEN",   0100000, 0100000 },
+	{ "EXTPROC",  0200000, 0200000 },
+};
+
+static void dumpflags(char* tag, int val, struct dict* bits, int nb)
+{
+	struct dict* p;
+	int n = 0;
+
+	printf("%s: ", tag);
+
+	for(p = bits; p < bits + nb; p++) {
+		if((val & p->mask) == p->bits) {
+			if(n++)
+				printf(" | ");
+			printf("%s", p->name);
+		}
+	}
+
+	printf("\n");
+}
+
+int main(void)
+{
+	struct winsize ws;
+	struct termios ts;
+	int ret, i;
+
+	if((ret = sys_ioctl(0, TIOCGWINSZ, &ws)) < 0)
+		fail("ioctl", "TIOCGWINSZ", ret);
+
+	printf("row=%i col=%i\n", ws.row, ws.col);
+
+	if((ret = sys_ioctl(0, TCGETS, &ts)) < 0)
+		fail("ioctl", "TCGETS", ret);
+
+	dumpflags("iflag", ts.iflag, iflags, ARRAY_SIZE(iflags));
+	dumpflags("oflag", ts.oflag, oflags, ARRAY_SIZE(oflags));
+	dumpflags("cflag", ts.cflag, cflags, ARRAY_SIZE(cflags));
+	dumpflags("lflag", ts.lflag, lflags, ARRAY_SIZE(lflags));
+
+	printf("ispeed=%i ospeed=%i\n", ts.ispeed, ts.ospeed);
+
+	printf("cc = { ");
+	for(i = 0; i < sizeof(ts.cc); i++) {
+		int q = ts.cc[i] & 0xFF;
+
+		if(i) printf(", ");
+
+		if(q >= 0x20 && q < 0x7F)
+			printf("'%c'", q);
+		else
+			printf("0x%02X", q);
+	}
+	printf(" }\n");
+
+	return 0;
+}
