6352b6d7eeae4e09948422720ebdf7160d2fd6e5
Alex Suykov <alex.suykov@gmail.com>
2017-08-28 10:46:07 +0300

    clean up arch bits

--- lib/arch/arm/bits/errno.h
+++ lib/arch/arm/bits/errno.h
@@ -1,135 +1,135 @@
 #ifndef __BITS_ERRNO_H__
 #define __BITS_ERRNO_H__
 
-#define EPERM		 1	/* Operation not permitted */
-#define ENOENT		 2	/* No such file or directory */
-#define ESRCH		 3	/* No such process */
-#define EINTR		 4	/* Interrupted system call */
-#define EIO		 5	/* I/O error */
-#define ENXIO		 6	/* No such device or address */
-#define E2BIG		 7	/* Arg list too long */
-#define ENOEXEC		 8	/* Exec format error */
-#define EBADF		 9	/* Bad file number */
-#define ECHILD		10	/* No child processes */
-#define EAGAIN		11	/* Try again */
-#define ENOMEM		12	/* Out of memory */
-#define EACCES		13	/* Permission denied */
-#define EFAULT		14	/* Bad address */
-#define ENOTBLK		15	/* Block device required */
-#define EBUSY		16	/* Device or resource busy */
-#define EEXIST		17	/* File exists */
-#define EXDEV		18	/* Cross-device link */
-#define ENODEV		19	/* No such device */
-#define ENOTDIR		20	/* Not a directory */
-#define EISDIR		21	/* Is a directory */
-#define EINVAL		22	/* Invalid argument */
-#define ENFILE		23	/* File table overflow */
-#define EMFILE		24	/* Too many open files */
-#define ENOTTY		25	/* Not a typewriter */
-#define ETXTBSY		26	/* Text file busy */
-#define EFBIG		27	/* File too large */
-#define ENOSPC		28	/* No space left on device */
-#define ESPIPE		29	/* Illegal seek */
-#define EROFS		30	/* Read-only file system */
-#define EMLINK		31	/* Too many links */
-#define EPIPE		32	/* Broken pipe */
-#define EDOM		33	/* Math argument out of domain of func */
-#define ERANGE		34	/* Math result not representable */
-#define EDEADLK		35	/* Resource deadlock would occur */
-#define ENAMETOOLONG	36	/* File name too long */
-#define ENOLCK		37	/* No record locks available */
-#define ENOSYS		38	/* Function not implemented */
-#define ENOTEMPTY	39	/* Directory not empty */
-#define ELOOP		40	/* Too many symbolic links encountered */
-#define EWOULDBLOCK	EAGAIN	/* Operation would block */
-#define ENOMSG		42	/* No message of desired type */
-#define EIDRM		43	/* Identifier removed */
-#define ECHRNG		44	/* Channel number out of range */
-#define EL2NSYNC	45	/* Level 2 not synchronized */
-#define EL3HLT		46	/* Level 3 halted */
-#define EL3RST		47	/* Level 3 reset */
-#define ELNRNG		48	/* Link number out of range */
-#define EUNATCH		49	/* Protocol driver not attached */
-#define ENOCSI		50	/* No CSI structure available */
-#define EL2HLT		51	/* Level 2 halted */
-#define EBADE		52	/* Invalid exchange */
-#define EBADR		53	/* Invalid request descriptor */
-#define EXFULL		54	/* Exchange full */
-#define ENOANO		55	/* No anode */
-#define EBADRQC		56	/* Invalid request code */
-#define EBADSLT		57	/* Invalid slot */
-#define EDEADLOCK	EDEADLK
-#define EBFONT		59	/* Bad font file format */
-#define ENOSTR		60	/* Device not a stream */
-#define ENODATA		61	/* No data available */
-#define ETIME		62	/* Timer expired */
-#define ENOSR		63	/* Out of streams resources */
-#define ENONET		64	/* Machine is not on the network */
-#define ENOPKG		65	/* Package not installed */
-#define EREMOTE		66	/* Object is remote */
-#define ENOLINK		67	/* Link has been severed */
-#define EADV		68	/* Advertise error */
-#define ESRMNT		69	/* Srmount error */
-#define ECOMM		70	/* Communication error on send */
-#define EPROTO		71	/* Protocol error */
-#define EMULTIHOP	72	/* Multihop attempted */
-#define EDOTDOT		73	/* RFS specific error */
-#define EBADMSG		74	/* Not a data message */
-#define EOVERFLOW	75	/* Value too large for defined data type */
-#define ENOTUNIQ	76	/* Name not unique on network */
-#define EBADFD		77	/* File descriptor in bad state */
-#define EREMCHG		78	/* Remote address changed */
-#define ELIBACC		79	/* Can not access a needed shared library */
-#define ELIBBAD		80	/* Accessing a corrupted shared library */
-#define ELIBSCN		81	/* .lib section in a.out corrupted */
-#define ELIBMAX		82	/* Attempting to link in too many shared libraries */
-#define ELIBEXEC	83	/* Cannot exec a shared library directly */
-#define EILSEQ		84	/* Illegal byte sequence */
-#define ERESTART	85	/* Interrupted system call should be restarted */
-#define ESTRPIPE	86	/* Streams pipe error */
-#define EUSERS		87	/* Too many users */
-#define ENOTSOCK	88	/* Socket operation on non-socket */
-#define EDESTADDRREQ	89	/* Destination address required */
-#define EMSGSIZE	90	/* Message too long */
-#define EPROTOTYPE	91	/* Protocol wrong type for socket */
-#define ENOPROTOOPT	92	/* Protocol not available */
-#define EPROTONOSUPPORT	93	/* Protocol not supported */
-#define ESOCKTNOSUPPORT	94	/* Socket type not supported */
-#define EOPNOTSUPP	95	/* Operation not supported on transport endpoint */
-#define ENOTSUP		EOPNOTSUPP/* Operation not supported on transport endpoint */
-#define EPFNOSUPPORT	96	/* Protocol family not supported */
-#define EAFNOSUPPORT	97	/* Address family not supported by protocol */
-#define EADDRINUSE	98	/* Address already in use */
-#define EADDRNOTAVAIL	99	/* Cannot assign requested address */
-#define ENETDOWN	100	/* Network is down */
-#define ENETUNREACH	101	/* Network is unreachable */
-#define ENETRESET	102	/* Network dropped connection because of reset */
-#define ECONNABORTED	103	/* Software caused connection abort */
-#define ECONNRESET	104	/* Connection reset by peer */
-#define ENOBUFS		105	/* No buffer space available */
-#define EISCONN		106	/* Transport endpoint is already connected */
-#define ENOTCONN	107	/* Transport endpoint is not connected */
-#define ESHUTDOWN	108	/* Cannot send after transport endpoint shutdown */
-#define ETOOMANYREFS	109	/* Too many references: cannot splice */
-#define ETIMEDOUT	110	/* Connection timed out */
-#define ECONNREFUSED	111	/* Connection refused */
-#define EHOSTDOWN	112	/* Host is down */
-#define EHOSTUNREACH	113	/* No route to host */
-#define EALREADY	114	/* Operation already in progress */
-#define EINPROGRESS	115	/* Operation now in progress */
-#define ESTALE		116	/* Stale NFS file handle */
-#define EUCLEAN		117	/* Structure needs cleaning */
-#define ENOTNAM		118	/* Not a XENIX named type file */
-#define ENAVAIL		119	/* No XENIX semaphores available */
-#define EISNAM		120	/* Is a named type file */
-#define EREMOTEIO	121	/* Remote I/O error */
-#define EDQUOT		122	/* Quota exceeded */
-#define ENOMEDIUM	123	/* No medium found */
-#define EMEDIUMTYPE	124	/* Wrong medium type */
-#define	ECANCELED	125	/* Operation Canceled */
-#define	ENOKEY		126	/* Required key not available */
-#define	EKEYEXPIRED	127	/* Key has expired */
-#define	EKEYREVOKED	128	/* Key has been revoked */
-#define	EKEYREJECTED	129	/* Key was rejected by service */
+#define EPERM            1
+#define ENOENT           2
+#define ESRCH            3
+#define EINTR            4
+#define EIO              5
+#define ENXIO            6
+#define E2BIG            7
+#define ENOEXEC          8
+#define EBADF            9
+#define ECHILD          10
+#define EAGAIN          11
+#define ENOMEM          12
+#define EACCES          13
+#define EFAULT          14
+#define ENOTBLK         15
+#define EBUSY           16
+#define EEXIST          17
+#define EXDEV           18
+#define ENODEV          19
+#define ENOTDIR         20
+#define EISDIR          21
+#define EINVAL          22
+#define ENFILE          23
+#define EMFILE          24
+#define ENOTTY          25
+#define ETXTBSY         26
+#define EFBIG           27
+#define ENOSPC          28
+#define ESPIPE          29
+#define EROFS           30
+#define EMLINK          31
+#define EPIPE           32
+#define EDOM            33
+#define ERANGE          34
+#define EDEADLK         35
+#define ENAMETOOLONG    36
+#define ENOLCK          37
+#define ENOSYS          38
+#define ENOTEMPTY       39
+#define ELOOP           40
+#define EWOULDBLOCK     EAGAIN
+#define ENOMSG          42
+#define EIDRM           43
+#define ECHRNG          44
+#define EL2NSYNC        45
+#define EL3HLT          46
+#define EL3RST          47
+#define ELNRNG          48
+#define EUNATCH         49
+#define ENOCSI          50
+#define EL2HLT          51
+#define EBADE           52
+#define EBADR           53
+#define EXFULL          54
+#define ENOANO          55
+#define EBADRQC         56
+#define EBADSLT         57
+#define EDEADLOCK       EDEADLK
+#define EBFONT          59
+#define ENOSTR          60
+#define ENODATA         61
+#define ETIME           62
+#define ENOSR           63
+#define ENONET          64
+#define ENOPKG          65
+#define EREMOTE         66
+#define ENOLINK         67
+#define EADV            68
+#define ESRMNT          69
+#define ECOMM           70
+#define EPROTO          71
+#define EMULTIHOP       72
+#define EDOTDOT         73
+#define EBADMSG         74
+#define EOVERFLOW       75
+#define ENOTUNIQ        76
+#define EBADFD          77
+#define EREMCHG         78
+#define ELIBACC         79
+#define ELIBBAD         80
+#define ELIBSCN         81
+#define ELIBMAX         82
+#define ELIBEXEC        83
+#define EILSEQ          84
+#define ERESTART        85
+#define ESTRPIPE        86
+#define EUSERS          87
+#define ENOTSOCK        88
+#define EDESTADDRREQ    89
+#define EMSGSIZE        90
+#define EPROTOTYPE      91
+#define ENOPROTOOPT     92
+#define EPROTONOSUPPORT 93
+#define ESOCKTNOSUPPORT 94
+#define EOPNOTSUPP      95
+#define ENOTSUP         EOPNOTSUPP
+#define EPFNOSUPPORT    96
+#define EAFNOSUPPORT    97
+#define EADDRINUSE      98
+#define EADDRNOTAVAIL   99
+#define ENETDOWN        100
+#define ENETUNREACH     101
+#define ENETRESET       102
+#define ECONNABORTED    103
+#define ECONNRESET      104
+#define ENOBUFS         105
+#define EISCONN         106
+#define ENOTCONN        107
+#define ESHUTDOWN       108
+#define ETOOMANYREFS    109
+#define ETIMEDOUT       110
+#define ECONNREFUSED    111
+#define EHOSTDOWN       112
+#define EHOSTUNREACH    113
+#define EALREADY        114
+#define EINPROGRESS     115
+#define ESTALE          116
+#define EUCLEAN         117
+#define ENOTNAM         118
+#define ENAVAIL         119
+#define EISNAM          120
+#define EREMOTEIO       121
+#define EDQUOT          122
+#define ENOMEDIUM       123
+#define EMEDIUMTYPE     124
+#define ECANCELED       125
+#define ENOKEY          126
+#define EKEYEXPIRED     127
+#define EKEYREVOKED     128
+#define EKEYREJECTED    129
 
 #endif
--- lib/arch/arm/bits/signal.h
+++ lib/arch/arm/bits/signal.h
@@ -1,40 +1,40 @@
 #ifndef __BITS_SIGNAL_H__
 #define __BITS_SIGNAL_H__
 
-#define SIGHUP		 1
-#define SIGINT		 2
-#define SIGQUIT		 3
-#define SIGILL		 4
-#define SIGTRAP		 5
-#define SIGABRT		 6
-#define SIGIOT		 6
-#define SIGBUS		 7
-#define SIGFPE		 8
-#define SIGKILL		 9
-#define SIGUSR1		10
-#define SIGSEGV		11
-#define SIGUSR2		12
-#define SIGPIPE		13
-#define SIGALRM		14
-#define SIGTERM		15
-#define SIGSTKFLT	16
-#define SIGCHLD		17
-#define SIGCONT		18
-#define SIGSTOP		19
-#define SIGTSTP		20
-#define SIGTTIN		21
-#define SIGTTOU		22
-#define SIGURG		23
-#define SIGXCPU		24
-#define SIGXFSZ		25
-#define SIGVTALRM	26
-#define SIGPROF		27
-#define SIGWINCH	28
-#define SIGIO		29
-#define SIGPOLL		SIGIO
-#define SIGPWR		30
-#define SIGSYS		31
-#define	SIGUNUSED	31
+#define SIGHUP           1
+#define SIGINT           2
+#define SIGQUIT          3
+#define SIGILL           4
+#define SIGTRAP          5
+#define SIGABRT          6
+#define SIGIOT           6
+#define SIGBUS           7
+#define SIGFPE           8
+#define SIGKILL          9
+#define SIGUSR1         10
+#define SIGSEGV         11
+#define SIGUSR2         12
+#define SIGPIPE         13
+#define SIGALRM         14
+#define SIGTERM         15
+#define SIGSTKFLT       16
+#define SIGCHLD         17
+#define SIGCONT         18
+#define SIGSTOP         19
+#define SIGTSTP         20
+#define SIGTTIN         21
+#define SIGTTOU         22
+#define SIGURG          23
+#define SIGXCPU         24
+#define SIGXFSZ         25
+#define SIGVTALRM       26
+#define SIGPROF         27
+#define SIGWINCH        28
+#define SIGIO           29
+#define SIGPOLL         SIGIO
+#define SIGPWR          30
+#define SIGSYS          31
+#define SIGUNUSED       31
 
 #define SIGRTMIN        32
 #define SIGRTMAX        63
@@ -46,15 +46,15 @@ typedef struct {
 
 #define EMPTYSIGSET { 0, 0 }
 
-#define SA_NOCLDSTOP	0x00000001
-#define SA_NOCLDWAIT	0x00000002
-#define SA_SIGINFO	0x00000004
-#define SA_THIRTYTWO	0x02000000
-#define SA_RESTORER	0x04000000
-#define SA_ONSTACK	0x08000000
-#define SA_RESTART	0x10000000
-#define SA_NODEFER	0x40000000
-#define SA_RESETHAND	0x80000000
+#define SA_NOCLDSTOP    0x00000001
+#define SA_NOCLDWAIT    0x00000002
+#define SA_SIGINFO      0x00000004
+#define SA_THIRTYTWO    0x02000000
+#define SA_RESTORER     0x04000000
+#define SA_ONSTACK      0x08000000
+#define SA_RESTART      0x10000000
+#define SA_NODEFER      0x40000000
+#define SA_RESETHAND    0x80000000
 
 struct sigaction {
 	union {
