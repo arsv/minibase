98b57211ea61d5287e27055655f0552422f6920b
Alex Suykov <alex.suykov@gmail.com>
2017-09-01 17:33:54 +0300

    lib: makedev()

--- lib/bits/major.h
+++ lib/bits/major.h
@@ -17,3 +17,12 @@ static inline long minor(uint64_t dev)
 	return ((dev >>  0) & 0x000000FF)
 	     | ((dev >> 12) & 0xFFFFFF00);
 }
+
+static inline uint64_t makedev(long maj, long min)
+{
+	return (((maj)&0xfffff000ULL) << 32) |
+	       (((maj)&0x00000fffULL) << 8) |
+	       (((min)&0xffffff00ULL) << 12) |
+	       (((min)&0x000000ffULL));
+
+}
