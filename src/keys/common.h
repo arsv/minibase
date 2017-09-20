bd73efb2ee1a0171c680504eb6647345320e2752
Alex Suykov <alex.suykov@gmail.com>
2017-09-20 23:29:35 +0300

    split input/ into keymon and udev stuff

--- src/keys/common.h
+++ src/keys/common.h
@@ -2,6 +2,3 @@
 
 #define CONFDIR ETCDIR "/keymon"
 #define CONFIG  ETCDIR "/keyacts"
-
-#define RUNUDEV     RUNDIR "/udev"
-#define RUNUDEVDATA RUNDIR "/udev/data"
---
+++ src/udev/common.h
@@ -0,0 +1,4 @@
+#include <dirs.h>
+
+#define RUNUDEV     RUNDIR "/udev"
+#define RUNUDEVDATA RUNDIR "/udev/data"
