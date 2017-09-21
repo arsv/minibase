9592322a47c826b6b262115a54134a2038f3abe3
Alex Suykov <alex.suykov@gmail.com>
2017-09-22 01:02:12 +0300

    udevmod: pipe to modprobe during initial device scan

--- src/udev/udevmod.c
+++ src/udev/udevmod.c
@@ -6,6 +6,7 @@
 #include <sys/fpath.h>
 #include <sys/dents.h>
 #include <sys/socket.h>
+#include <sys/signal.h>
 
 #include <errtag.h>
 #include <printf.h>
@@ -22,6 +23,9 @@ ERRTAG("udevmod");
 struct top {
 	int udev;
 	char** envp;
+
+	int fd;  /* of a running modprobe -p process */
+	int pid;
 };
 
 struct rfn {
@@ -34,7 +38,57 @@ struct rfn {
 #define FN struct rfn* fn
 #define AT(dd) dd->at, dd->name
 
-static void modprobe(CTX, char* name)
+/* During initial device scan, udevmod will try dozens of modaliases
+   in quick succession, most of them invalid. There is no point in
+   spawning that many modprobe processes. Instead, we spawn one and
+   pipe it aliases to check.
+
+   After the initial scan, this becomes pointless since udev events
+   are rare and the ones with modaliases tend to arrive one at a time,
+   so we switch to spawning modprobe on each event. */
+
+static void open_modprobe(CTX)
+{
+	int fds[2];
+	int ret, pid;
+
+	if((ret = sys_pipe(fds)) < 0)
+		fail("pipe", NULL, ret);
+	if((pid = sys_fork()) < 0)
+		fail("fork", NULL, pid);
+
+	if(pid == 0) {
+		char* argv[] = { "/sbin/modprobe", "-qbp", NULL };
+		sys_dup2(fds[0], STDIN);
+		sys_close(fds[1]);
+		ret = sys_execve(*argv, argv, ctx->envp);
+		fail("execve", *argv, ret);
+	}
+
+	sys_close(fds[0]);
+
+	ctx->pid = pid;
+	ctx->fd = fds[1];
+}
+
+static void stop_modprobe(CTX)
+{
+	int pid = ctx->pid;
+	int ret, status;
+	int fd = ctx->fd;
+
+	if(fd < 0) fd = -fd;
+
+	sys_close(fd);
+
+	if((ret = sys_waitpid(pid, &status, 0)) < 0)
+		fail("waitpid", NULL, ret);
+
+	ctx->fd = -1;
+	ctx->pid = 0;
+}
+
+static void run_modprobe(CTX, char* name)
 {
 	int pid, ret, status;
 
@@ -44,13 +98,38 @@ static void modprobe(CTX, char* name)
 	}
 
 	if(pid == 0) {
-		char* argv[] = { "/sbin/modprobe", "-q", name, NULL };
+		char* argv[] = { "/sbin/modprobe", "-qb", name, NULL };
 		ret = sys_execve(*argv, argv, ctx->envp);
 		fail("execve", *argv, ret);
 	} else {
 		if((ret = sys_waitpid(pid, &status, 0)) < 0)
 			warn("waitpid", NULL, ret);
 	}
+}
+
+static void out_modprobe(CTX, char* name)
+{
+	int fd = ctx->fd;
+	int nlen = strlen(name);
+	int ret;
+
+	if(fd <= 0) return;
+
+	name[nlen] = '\n';
+
+	ret = sys_write(ctx->fd, name, nlen + 1);
+
+	name[nlen] = '\0';
+
+	if(ret == -EPIPE) ctx->fd = -ctx->fd;
+}
+
+static void modprobe(CTX, char* name)
+{
+	if(ctx->pid)
+		out_modprobe(ctx, name);
+	else
+		run_modprobe(ctx, name);
 }
 
 static char* get_modalias(char* buf, int len)
@@ -208,6 +287,17 @@ static void scan_devices(CTX)
 	scan_dir(ctx, &start);
 }
 
+static void suppress_sigpipe(void)
+{
+	struct sigaction sa = {
+		.handler = SIG_IGN,
+		.flags = 0,
+		.restorer = NULL
+	};
+
+	sys_sigaction(SIGPIPE, &sa, NULL);
+}
+
 int main(int argc, char** argv, char** envp)
 {
 	struct top context, *ctx = &context;
@@ -220,7 +310,11 @@ int main(int argc, char** argv, char** envp)
 	ctx->envp = envp;
 
 	open_udev(ctx);
+
+	suppress_sigpipe();
+	open_modprobe(ctx);
 	scan_devices(ctx);
+	stop_modprobe(ctx);
 
 	while(1) recv_event(ctx);
 }
