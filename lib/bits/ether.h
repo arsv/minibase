39fff5f131b6e01545b244f8f4d522433ea84067
Alex Suykov <alex.suykov@gmail.com>
2017-10-17 17:07:42 +0300

    lib: bits/ether.h

---
+++ lib/bits/ether.h
@@ -0,0 +1,14 @@
+#ifndef __BITS_ETHER_H__
+#define __BITS_ETHER_H__
+
+#include <bits/types.h>
+
+#define ETH_P_PAE 0x888E
+
+struct ethhdr {
+	byte dst[6];
+	byte src[6];
+	uint16_t proto;
+} __attribute__((packed));
+
+#endif
