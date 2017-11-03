41aa7c1d08d9092f4a9d8d426b07673ceb7e0eb4
Alex Suykov <alex.suykov@gmail.com>
2017-11-03 19:12:30 +0200

    wsupp: comments on rfkill

--- src/net2/wsupp_rfkill.c
+++ src/net2/wsupp_rfkill.c
@@ -18,35 +18,34 @@
    (and this tool doesn't even listen to RTNL notifications).
 
    The only somewhat reliable way to be notified is by listening to /dev/rfkill.
-   Now rfkill device however is provided by a standalone module that may not be
-   loadeded at any given time, and may get un-/re-loaded. Normally this does not
-   happens, so wimon keeps the fd open. However if open attempt fails, wsupp
-   will try to re-open it on any suitable occasion. This may lead to redundant
-   open calls in case rfkill is in fact missing, but there's probably no other
-   way around this. Hopefully rfkill events are rare.
+   But rfkill device is provided by a standalone module that may not be loadeded
+   at any given time, and may get un-/re-loaded. Normally this does not happens,
+   so wimon keeps the fd open. However if open attempt fails, wsupp will try to
+   re-open it on any suitable occasion. This may lead to redundant open calls
+   in case rfkill is in fact missing, but there's probably no other way around
+   this. Hopefully events that trigger reopen attempts are rare.
 
    Another problem is that /dev/rfkill reports events for rfkill devices (idx
    in the struct below) which do *not* match netdev ifi-s. The trick used here
-   is to check /sys/class/net/$ifname/phy80211/rfkill$idx whenever any relevant
-   event arrives. The $idx-$ifname association seems to be stable for at least
-   as long as the fd remains open, but there are no guarantees beyond that.
+   is to check for /sys/class/net/$ifname/phy80211/rfkill$idx. The $idx-$ifname
+   association seems to be stable for at least as long as the fd remains open,
+   but there are no guarantees beyond that.
 
-   The end result of this all is effectively "ifconfig (iface) up" being run
-   each time some managed link gets un-killed. Link state change gets picked up
-   by RTNL code, which triggers link_enabled, which in turn proceeds to restore
-   wifi connection if necessary. */
+   The end result of this all is effectively `ifconfig (iface) up` done each
+   time some managed link gets un-killed, followed by attempt to re-establish
+   connection. */
 
 int rfkill;
 int rfkilled;
 static int rfkidx;
 
-/* The interface gets brought up with simple ioctls. It could have been
-   done with RTNL as well, but setting up RTNL for this mere reason
-   hardly makes sense.
+/* The interface gets brought up with simple ioctls. It could have been done
+   with RTNL as well, but setting up RTNL for this mere reason hardly makes
+   sense.
 
-   Current code works on interface named $ifname, in contrast with GENL
-   code that uses $ifindex instead. Renaming the interface while wsupp
-   runs *will* confuse it. */
+   Current code works on interface named $ifname, in contrast with GENL code
+   that uses $ifindex instead. Renaming the interface while wsupp runs *will*
+   confuse it. */
 
 #define IFF_UP (1<<0)
 
