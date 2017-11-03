710f1a69e25c541e53f785e14f4c008d5ed96b45
Alex Suykov <alex.suykov@gmail.com>
2017-11-03 17:25:16 +0200

    init: add badsrv
    Demo "service" to show up how supervised process output is handled
    and how restarting works in bootable images.

---
+++ src/init/badsrv.c
@@ -0,0 +1,22 @@
+#include <sys/file.h>
+#include <sys/sched.h>
+
+#include <string.h>
+
+static void say(char* msg)
+{
+	int len = strlen(msg);
+	sys_write(STDOUT, msg, len);
+}
+
+int main(void)
+{
+	struct timespec ts = { 1, 0 };
+
+	say("badsrv: starting\n");
+	say("badsrv: some output here\n");
+	sys_nanosleep(&ts, NULL);
+	say("badsrv: failed, exiting\n");
+
+	return 0xFF;
+}
