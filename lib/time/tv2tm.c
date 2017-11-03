74d289f0a5be22b1b50f111c1e3157d89089507a
Alex Suykov <alex.suykov@gmail.com>
2017-11-03 13:04:29 +0200

    license notice for the musl code

--- lib/time/tv2tm.c
+++ lib/time/tv2tm.c
@@ -6,7 +6,32 @@
    Musl calls it __secs_to_tm() internally.  */
 
 /* Code taken from musl. My oh-so-clever version naturally failed badly
-   on Feb 1, 2016 (leap year). Big surprise, yeah. */
+   on Feb 1, 2016 (leap year). Big surprise, yeah.
+
+   Original copyright notice follows:
+
+   ----------------------------------------------------------------------
+   Copyright © 2005-2014 Rich Felker, et al.
+   
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+   
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   ---------------------------------------------------------------------- */
 
 /* 2000-03-01 (mod 400 year, immediately after feb29 */
 #define LEAPOCH (946684800LL + 86400*(31+29))
