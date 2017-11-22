67e1e77bea466e4744837e6963da418b58c5d437
Alex Suykov <alex.suykov@gmail.com>
2017-11-22 23:53:30 +0200

    new build rules in src/

--- src/rules.mk
+++ src/rules.mk
@@ -1,7 +1,7 @@
 .SUFFIXES:
 .SECONDARY:
 
-/ := $(dir $(wildcard ../configure ../../configure ../../../configure))
+/ := ../../
 
 include $/config.mk
 
@@ -16,70 +16,59 @@ all:
 %: %.o
 	$(LD) -o $@ $(filter %.o,$^) $(LIBS)
 
-# Quick explanation, say for bin="ls echo":
-#
-#   dstdir-bin := ./out/bin
-#   target-bin := ./out/bin/ls ./out/bin/echo
-#   mkdirs += ./out/bin
-#
-#   ./out/bin/{ls,echo}: ./out/bin/%: % | ./out/bin
-#       strip -o $@ $<
-#
-#   ./out/bin:         # <-- actually via $(mkdirs)
-#       mkdir -p $@
-#
-# In actual runs DESTDIR is always an absolute path.
-
-define bin-rules
-dstdir-$1 := $$(DESTDIR)$$($1dir)
-target-$1 := $$(patsubst %,$$(dstdir-$1)/%,$$($1))
-mkdirs += $$(dstdir-$1)
-clean += $$($1)
-
-all: all-$1
-
-all-$1: $$($1)
-
-install: install-$1
-
-install-$1: $$(target-$1) | $$(dstdir-$1)
-
-$$(target-$1): $$(dstdir-$1)/%: % | $$(dstdir-$1)
-	$$(STRIP) -o $$@ $$(notdir $$@)
-
-.PHONY: $$(target-$1)
-endef
-
-targets = command service system initrd
-
-$(foreach _,$(targets),$(if $($_),$(eval $(call bin-rules,$_))))
-
-define man-rules
-dstdir-$1 := $$(DESTDIR)$$($1dir)
-target-$1 := $$(patsubst %,$$(dstdir-$1)/%,$$($1))
-mkdirs += $$(dstdir-$1)
-
-install: install-$1
-
-install-man: install-$1
-
-install-$1: $$(target-$1)
-
-$$(target-$1): $$(dstdir-$1)/%: % | $$(dstdir-$1)
-	cp -t $$(dir $$@) $$(notdir $$@)
-
-.PHONY: $$(target-$1)
-endef
-
-$(foreach _, 1 5 8, $(eval man$_ := $(sort $(wildcard *.$_))))
-$(foreach _, 1 5 8, $(if $(man$_),$(eval $(call man-rules,man$_,$_))))
-
-$(sort $(mkdirs)): %:
-	mkdir -p $@
-
-all: $(other)
-
-clean += $(other)
-
 clean:
 	rm -f $(clean)
+
+install: install-bin install-man
+
+install-bin:
+install-man:
+
+# --- Definitions
+define destdir
+dest$1dir := $$(DESTDIR)$$($1dir)
+
+$$(dest$1dir):
+	mkdir -p $$@
+endef
+
+$(eval $(call destdir,command))
+$(eval $(call destdir,service))
+$(eval $(call destdir,system))
+$(eval $(call destdir,initrd))
+$(eval $(call destdir,man1))
+$(eval $(call destdir,man5))
+$(eval $(call destdir,man8))
+
+# --- 1=service, 2=foo
+define register
+all: $2
+
+install-bin: install-$1-$2
+
+install-$1-$2: $2 | $$(dest$1dir)
+	$$(STRIP) -o $$(dest$1dir)/$2 $2
+
+clean += $2
+
+$$(if $3,$$(eval $$(call man,$2,$3)))
+endef
+
+service = $(call register,service,$1,$2)
+command = $(call register,command,$1,$2)
+initrd = $(call register,initrd,$1,$2)
+
+# --- other foo
+define other
+all: $1
+
+clean += $1
+endef
+
+# --- man foo 1
+define man
+install-man: install-man-$1
+
+install-man-$1: $1.$2 | $$(destman$2dir)
+	cp -t $$(destman$2dir) $1.$2
+endef
