8b23e9caaad0eab0b8ad66433848baf748dff592
Alex Suykov <alex.suykov@gmail.com>
2017-11-23 01:07:44 +0200

    drop /boot/sbin, add /sbin/cmdops

--- src/rules.mk
+++ src/rules.mk
@@ -35,7 +35,7 @@ endef
 $(eval $(call destdir,command))
 $(eval $(call destdir,service))
 $(eval $(call destdir,system))
-$(eval $(call destdir,initrd))
+$(eval $(call destdir,cmdops))
 $(eval $(call destdir,man1))
 $(eval $(call destdir,man5))
 $(eval $(call destdir,man8))
@@ -44,9 +44,9 @@ $(eval $(call destdir,man8))
 define register
 all: $2
 
-install-bin: install-$1-$2
+install-bin: install-$2
 
-install-$1-$2: $2 | $$(dest$1dir)
+install-$2: $2 | $$(dest$1dir)
 	$$(STRIP) -o $$(dest$1dir)/$2 $2
 
 clean += $2
@@ -56,7 +56,8 @@ endef
 
 service = $(call register,service,$1,$2)
 command = $(call register,command,$1,$2)
-initrd = $(call register,initrd,$1,$2)
+cmdops = $(call register,cmdops,$1,$2)
+system = $(call register,system,$1,$2)
 
 # --- other foo
 define other
@@ -67,8 +68,8 @@ endef
 
 # --- man foo 1
 define man
-install-man: install-man-$1
+install-man: install-$1.$2
 
-install-man-$1: $1.$2 | $$(destman$2dir)
+install-$1.$2: $1.$2 | $$(destman$2dir)
 	cp -t $$(destman$2dir) $1.$2
 endef
