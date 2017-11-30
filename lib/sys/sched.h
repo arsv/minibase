4c2cd017f2c8080e0f0b041fb93b933330054b2d
Alex Suykov <alex.suykov@gmail.com>
2017-12-01 00:41:31 +0200

    lib: sched_getaffinity and sched_setaffinity

--- lib/sys/sched.h
+++ lib/sys/sched.h
@@ -47,3 +47,43 @@ inline static long sys_setpriority(int which, int who, int nice)
 {
 	return syscall3(NR_setpriority, which, who, nice);
 }
+
+#if BITS == 32
+struct cpuset { ulong bits[32]; };
+#else
+struct cpuset { ulong bits[16]; };
+#endif
+
+inline static void cpuset_set(struct cpuset* mask, uint n)
+{
+	if(8*n > sizeof(mask->bits))
+		return;
+
+	int wordsize = 8*sizeof(mask->bits[0]);
+	int wordidx = n / wordsize;
+	int wordbit = n % wordsize;
+
+	mask->bits[wordidx] |= (1UL << wordbit);
+}
+
+inline static int cpuset_get(struct cpuset* mask, uint n)
+{
+	if(8*n > sizeof(mask->bits))
+		return 0;
+
+	int wordsize = 8*sizeof(mask->bits[0]);
+	int wordidx = n / wordsize;
+	int wordbit = n % wordsize;
+
+	return !!(mask->bits[wordidx] & (1UL << wordbit));
+}
+
+inline static long sys_sched_getaffinity(int pid, struct cpuset* mask)
+{
+	return syscall3(NR_sched_getaffinity, pid, sizeof(*mask), (long)mask);
+}
+
+inline static long sys_sched_setaffinity(int pid, struct cpuset* mask)
+{
+	return syscall3(NR_sched_setaffinity, pid, sizeof(*mask), (long)mask);
+}
