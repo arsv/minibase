62899118df0ee63e281cd99e9bbe033ffcb91b0f
Alex Suykov <alex.suykov@gmail.com>
2017-12-06 01:17:29 +0200

    lib: signalfd

--- lib/sys/signal.h
+++ lib/sys/signal.h
@@ -1,4 +1,6 @@
 #include <syscall.h>
+#include <bits/types.h>
+#include <bits/fcntl.h>
 #include <bits/signal.h>
 
 #define SIG_BLOCK	0
@@ -9,14 +11,38 @@
 #define SIG_IGN ((void*) 1L)
 #define SIG_ERR ((void*)~0L)
 
+#define SFD_NONBLOCK O_NONBLOCK
+#define SFD_CLOEXEC  O_CLOEXEC
+
+struct sigevent {
+	uint32_t signo;
+	 int32_t errno;
+	 int32_t code;
+	uint32_t pid;
+	uint32_t uid;
+	 int32_t fd;
+	uint32_t tid;
+	uint32_t band;
+	uint32_t overrun;
+	uint32_t trapno;
+	 int32_t status;
+	 int32_t intval;
+	uint64_t ptrval;
+	uint64_t utime;
+	uint64_t stime;
+	uint64_t addr;
+	uint16_t addr_lsb;
+	byte pad[46];
+} __attribute__((packed));
+
 extern void sigreturn(void);
 
-inline static long sys_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact)
+inline static long sys_sigaction(int signum, const struct sigaction* act, struct sigaction* oldact)
 {
 	return syscall4(NR_rt_sigaction, signum, (long)act, (long)oldact, sizeof(sigset_t));
 }
 
-inline static long sys_sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+inline static long sys_sigprocmask(int how, const sigset_t* set, sigset_t* oldset)
 {
 	return syscall4(NR_rt_sigprocmask, how, (long)set, (long)oldset, sizeof(*set));
 }
@@ -24,4 +50,9 @@ inline static long sys_sigprocmask(int how, const sigset_t *set, sigset_t *oldse
 inline static long sys_kill(int pid, int sig)
 {
 	return syscall2(NR_kill, pid, sig);
+}
+
+inline static long sys_signalfd(int fd, sigset_t* set, int flags)
+{
+	return syscall4(NR_signalfd4, fd, (long)set, sizeof(*set), flags);
 }
