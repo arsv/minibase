5c44d629d4ce64792ec47b0fe49e4994a978c26b
Alex Suykov <alex.suykov@gmail.com>
2017-12-24 19:49:54 +0200

    configure: selective tool configuration
    This introduces configure keyword "only" and allows for positive tool
    selection (listed tools are the only ones to be built).
    
    Before this, only limited negative selection was possible (skip named
    tools). Negative selection is easier to implement within minibase but
    otherwise makes little sense. Most users are expected to know which
    tools they are going to use.

--- src/rules.mk
+++ src/rules.mk
@@ -24,7 +24,7 @@ install: install-bin install-man
 install-bin:
 install-man:
 
-# --- Definitions
+# Common mkdir part for the several installation directories.
 define destdir
 dest$1dir := $$(DESTDIR)$$($1dir)
 
@@ -39,11 +39,16 @@ $(eval $(call destdir,man1))
 $(eval $(call destdir,man5))
 $(eval $(call destdir,man8))
 
-# --- 1=service, 2=foo
-define register
-all: $2
+# Conditional for the "only" config option; $1=foo, $2=result
+define ifenabled
+$(if $(only),$(if $(filter $1,$(only)),$2),$2)
+endef
 
-install-bin: install-$2
+# Define executable to be built and installed; $1=service, $2=foo
+define register
+all: $$(call ifenabled,$2,$2)
+
+install-bin: $$(call ifenabled,$2,install-$2)
 
 install-$2: $2 | $$(dest$1dir)
 	$$(STRIP) -o $$(dest$1dir)/$2 $2
@@ -53,18 +58,22 @@ clean += $2
 $$(if $3,$$(eval $$(call man,$2,$3)))
 endef
 
+# Registrable executables classes.
+# The only difference is installation directory really.
 service = $(call register,service,$1,$2)
 command = $(call register,command,$1,$2)
 system = $(call register,system,$1,$2)
 
-# --- other foo
+# Define executables that shoudl be built but not installed; $1=foo
+# (this is for auxillary development tools)
 define other
-all: $1
+all: $$(call ifenabled,$1,$1)
 
 clean += $1
 endef
 
-# --- man foo 1
+# Man pages. Mostly used via `register` but there are also a couple
+# of standalone section 5 pages.
 define man
 install-man: install-$1.$2
 
