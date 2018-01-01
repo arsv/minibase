fd9ed602c6c4d2b93d2b5d4cbfac17d1407c36ff
Alex Suykov <alex.suykov@gmail.com>
2018-01-01 16:00:23 +0200

    lib: swabx for 64-bit ints

--- lib/endian.h
+++ lib/endian.h
@@ -13,6 +13,18 @@ inline static uint32_t swabl(uint32_t n)
 	       | (((n >> 24) & 0xFF) <<  0);
 }
 
+inline static uint64_t swabx(uint64_t n)
+{
+	return   (((n >> 0*8) & 0xFF) << 7*8)
+	       | (((n >> 1*8) & 0xFF) << 6*8)
+	       | (((n >> 2*8) & 0xFF) << 5*8)
+	       | (((n >> 3*8) & 0xFF) << 4*8)
+	       | (((n >> 4*8) & 0xFF) << 3*8)
+	       | (((n >> 5*8) & 0xFF) << 2*8)
+	       | (((n >> 6*8) & 0xFF) << 1*8)
+	       | (((n >> 7*8) & 0xFF) << 0*8);
+}
+
 #ifdef BIGENDIAN
 
 inline static uint16_t htons(uint16_t n) { return n; }
