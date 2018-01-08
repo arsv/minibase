a9091909457b423a9766ec5f052de4738dbc9bbd
Alex Suykov <alex.suykov@gmail.com>
2018-01-08 16:34:59 +0200

    lib: fix ioctl macros to handle bit 32 properly

--- lib/bits/ioctl.h
+++ lib/bits/ioctl.h
@@ -1,16 +1,16 @@
 #ifndef __BITS_IOCTL_H__
 #define __BITS_IOCTL_H__
 
-#define _IOC_W	1
-#define _IOC_R	2
-#define _IOC_RW	3
+#define _IOC_W  1UL
+#define _IOC_R  2UL
+#define _IOC_RW 3UL
 
 #define _IOC(dir,type,nr,size) \
-	( ((dir)<<30) | ((type)<<8) | (nr) | ((size)<<16) )
+        ( ((dir)<<30) | ((type)<<8) | (nr) | ((size)<<16) )
 
-#define _IO(type,nr)		_IOC(0,(type),(nr),0)
-#define _IOR(type,nr,size)	_IOC(_IOC_R,(type),(nr),sizeof(size))
-#define _IOW(type,nr,size)	_IOC(_IOC_W,(type),(nr),sizeof(size))
-#define _IOWR(type,nr,size)	_IOC(_IOC_RW,(type),(nr),sizeof(size))
+#define _IO(type,nr)        _IOC(0,(type),(nr),0)
+#define _IOR(type,nr,size)  _IOC(_IOC_R,(type),(nr),sizeof(size))
+#define _IOW(type,nr,size)  _IOC(_IOC_W,(type),(nr),sizeof(size))
+#define _IOWR(type,nr,size) _IOC(_IOC_RW,(type),(nr),sizeof(size))
 
 #endif
