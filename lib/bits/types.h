41a1aca960e127a8b9a0a9939dc943def6fb3a37
Alex Suykov <alex.suykov@gmail.com>
2018-01-22 22:49:37 +0200

    move mem_off_cmp to cdefs.h

--- lib/bits/types.h
+++ lib/bits/types.h
@@ -43,45 +43,4 @@ typedef long time_t;
 typedef unsigned long size_t;
 typedef int64_t off_t;
 
-/* Let's take a moment to realize the significance of the two lines above.
-   Size of stuff in memory is always unsigned, while size of stuff on disk
-   is signed. This *will* trigger any strict signed-unsigned checks in a way
-   that *cannot* be avoided. Ad-hoc casts to fix the warnings will only make
-   the situation worse: 32 < 63 on 32-bit arches but 64 > 63 on 64-bit arches.
-
-   The following is a reasonably safe way to avoid signed-unsigned compare
-   warnings. It will not help with assignments however, those must be handled
-   *very* carefully.
-
-   The proper way to fix this is to make file sizes unsigned and change seek
-   modes to { SEEK_SET, SEEK_FWD, SEEK_REV, SEEK_END }, all taking unsigned
-   offsets. Too bad the wrong decision is already burned into the syscall
-   interface.
-
-   Return: -1 if msz < fsz, +1 if msz > fsz. */
-
-inline static int mem_off_cmp(size_t msz, off_t fsz)
-{
-	if(fsz < 0)
-		return 1;
-
-#if BITS == 32 /* sizeof(msz) < sizeof(fsz) */
-	off_t smsz = (off_t)msz;
-
-	if(smsz < fsz)
-		return -1;
-	if(smsz > fsz)
-		return 1;
-#else /* sizeof(msz) == sizeof(fsz) */
-	size_t ufsz = (size_t)fsz;
-
-	if(msz < ufsz)
-		return -1;
-	if(msz > ufsz)
-		return 1;
-#endif
-
-	return 0;
-}
-
 #endif
