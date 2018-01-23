6a83444025b8a8e62c6c553cd5bc22577bbef522
Alex Suykov <alex.suykov@gmail.com>
2018-01-23 18:36:56 +0200

    test stdarg

---
+++ test/format/stdarg.c
@@ -0,0 +1,48 @@
+#include <stdarg.h>
+#include <format.h>
+#include <util.h>
+
+static void check(int exp, int got)
+{
+	FMTBUF(p, e, buf, 100);
+
+	p = fmtstr(p, e, __FILE__);
+	p = fmtstr(p, e, ":");
+
+	if(exp == got) {
+		p = fmtstr(p, e, " OK ");
+		p = fmtint(p, e, got);
+	} else {
+		p = fmtstr(p, e, " FAIL ");
+		p = fmtint(p, e, got);
+		p = fmtstr(p, e, " expected ");
+		p = fmtint(p, e, exp);
+	}
+
+	FMTENL(p, e);
+
+	writeall(STDERR, buf, p - buf);
+}
+
+static void ints(const char* fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	
+	check(1, va_arg(ap, int));
+	check(2, va_arg(ap, int));
+	check(3, va_arg(ap, int));
+	check(4, va_arg(ap, int));
+	check(5, va_arg(ap, int));
+	check(6, va_arg(ap, int));
+
+	va_end(ap);
+}
+
+int main(void)
+{
+	ints("foo", 1, 2, 3, 4, 5, 6);
+
+	return 0;
+}
