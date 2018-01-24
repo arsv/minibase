02566c999c31ae1eb4b571f1d4c47f414654dd01
Alex Suykov <alex.suykov@gmail.com>
2018-01-24 16:25:04 +0200

    lib: declare vfdprintf
    Useful for tests.

--- lib/printf.h
+++ lib/printf.h
@@ -1,9 +1,11 @@
 #include <bits/types.h>
+#include <stdarg.h>
 
 #define _format(n,m) __attribute__((format(printf,n,m)))
 
 int printf(const char* fmt, ...) _format(1,2);
 int tracef(const char* fmt, ...) _format(1,2);
 int snprintf(char* buf, ulong len, const char* fmt, ...) _format(3,4);
+int vfdprintf(int fd, const char* fmt, va_list ap);
 
 #undef _format
