a21dc22f954ea362ec928e8c89ef59d9a9583777
Alex Suykov <alex.suykov@gmail.com>
2018-03-04 23:34:09 +0200

    lib: fmtsize should round sizes to nearest

--- lib/format/fmtsize.c
+++ lib/format/fmtsize.c
@@ -1,40 +1,48 @@
 #include <bits/types.h>
+#include <printf.h>
 #include <format.h>
-
-static char* fmt1i0(char* p, char* e, int n)
-{
-	if(p < e)
-		*p++ = '0' + (n % 10);
-	return p;
-}
 
 char* fmtsize(char* p, char* e, uint64_t n)
 {
 	static const char sfx[] = " KMGTP";
+	static const unsigned maxi = sizeof(sfx) - 2; /* stop at P */
 	unsigned sfi = 0;
 	unsigned fr = 0;
 
 	/* find out the largest multiplier we can use */
-	for(; sfi < sizeof(sfx) && n > 1024; sfi++) {
+	while(n >= 1024 && sfi < maxi) {
 		fr = n % 1024;
 		n /= 1024;
+		sfi++;
 	}
-	
-	if(sfi >= sizeof(sfx)) {
-		/* Too large; format the number and be done with it. */
-		p = fmtu64(p, e, (uint32_t)n);
-		p = fmtchar(p, e, sfx[sizeof(sfx)-1]);
-	} else {
-		/* Manageable; do nnnn.d conversion */
-		p = fmti64(p, e, n);
 
-		if(sfi > 1 && n < 10) {
-			/* No decimals for bytes and Kbytes */
-			fr = fr*10/1024; /* one decimal */
+	if(sfi < 1) { /* bytes */
+		return fmtu64(p, e, n);
+	} else if(sfi == 1) { /* KB; round to nearest integer */
+		if(fr >= 512) n++;
+		p = fmtu64(p, e, n);
+		p = fmtchar(p, e, sfx[sfi]);
+	} else if(n < 1024) { /* MB to PB */
+		if(n >= 10) { /* round to nearest integer for large n */
+			if(fr >= 512)
+				n++;
+			p = fmti64(p, e, n);
+		} else { /* only show decimals if the n is short */
+			fr = ((fr*100)/1024 + 5)/10;
+			if(fr >= 10) {
+				n++;
+				fr = 0;
+			}
+			p = fmti64(p, e, n);
 			p = fmtchar(p, e, '.');
-			p = fmt1i0(p, e, fr);
-		} if(sfi > 0)
-			p = fmtchar(p, e, sfx[sfi]);
+			p = fmtchar(p, e, '0' + fr);
+		}
+
+		p = fmtchar(p, e, sfx[sfi]);
+	} else { /* Too large; return suffixed integer. */
+		if(fr >= 512) n++;
+		p = fmtu64(p, e, n);
+		p = fmtchar(p, e, sfx[maxi]);
 	}
 
 	return p;
