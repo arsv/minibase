2d7d0b992b86f9f998e2ada7b2749350309a7a12
Alex Suykov <alex.suykov@gmail.com>
2018-04-22 00:02:51 +0300

    lib: fix strnstr

--- lib/string/strnstr.c
+++ lib/string/strnstr.c
@@ -4,20 +4,22 @@ char* strnstr(const char* str, const char* sub, size_t len)
 {
 	size_t sublen = strlen(sub);
 
-	const char* p = str;
-	const char* q = str;
-	const char* r;
-
-	const char* end = str + len - sublen;
-
-	if(end <= str)
+	if(sublen > len)
 		return NULL;
 
-	for(p = str; p < end; p++) {
-		for(r = p, q = sub; *q && *q == *r; r++, q++)
-			;
-		if(!*q)
-			return (char*) p;
+	const char* pos = str;
+	const char* end = str + len - sublen;
+
+	for(pos = str; pos <= end; pos++) {
+		const char* p = pos;
+		const char* q = sub;
+
+		while(*q && *q == *p) {
+			p++;
+			q++;
+		} if(!*q) {
+			return (char*) pos;
+		}
 	}
 
 	return NULL;
