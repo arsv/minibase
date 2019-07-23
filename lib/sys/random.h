7734945bad45eddab4b1aa41eba4bcc265e61d07
Alex Suykov <alex.suykov@gmail.com>
2019-07-23 11:39:38 +0300

    lib: getrandom flags

--- lib/sys/random.h
+++ lib/sys/random.h
@@ -1,5 +1,8 @@
 #include <syscall.h>
 
+#define GRND_NONBLOCK   (1<<0)
+#define GRND_RANDOM     (1<<1)
+
 inline static long sys_getrandom(void* buf, size_t len, int flags)
 {
 	return syscall3(NR_getrandom, (long)buf, len, flags);
