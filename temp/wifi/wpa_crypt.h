e74cf250e8979e6b96a1711b67bf1f2cd53c2197
Alex Suykov <alex.suykov@gmail.com>
2019-09-16 20:31:01 +0300

    temp remove wpa tool
    This was useful in early stages, but at this point it's way behind
    wsupp, and requires a complete rewrite to be of any use again.
    
    Maybe I'll restore it later but for now there's no pressing need to
    do so and no spare time either.

--- temp/wifi/wpa_crypt.h
+++
@@ -1,9 +0,0 @@
-#include <bits/types.h>
-
-void PRF480(uint8_t out[60], uint8_t key[32], char* str,
-            uint8_t mac1[6], uint8_t mac2[6],
-            uint8_t nonce1[32], uint8_t nonce2[32]);
-
-void make_mic(uint8_t mic[16], uint8_t kck[16], char* buf, int len);
-int check_mic(uint8_t mic[16], uint8_t kck[16], char* buf, int len);
-int unwrap_key(uint8_t kek[16], char* buf, int len);
