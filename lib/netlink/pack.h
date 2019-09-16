d12f6b8c8d8d9be2375364f545197ffdd04a9491
Alex Suykov <alex.suykov@gmail.com>
2019-09-17 02:20:48 +0300

    lib: NL comments

--- lib/netlink/pack.h
+++ lib/netlink/pack.h
@@ -1,5 +1,40 @@
 #include <bits/types.h>
 
+/* NL command buffer, and routines for packing outbound messages.
+
+   Intended usage for GENL:
+
+	struct ncbuf nc;
+
+	nc_buf_set(&nc, buf, sizeof(buf));
+
+	nc_header(&nc, family, flags, seq);
+	nc_gencmd(&nc, command, version);
+	nc_put_...(&nc, ...);
+	nc_put_...(&nc, ...);
+
+	if((ret = nc_send(fd, &nc)) < 0)
+		fail(...);
+
+   RTNL messages need custom headers:
+
+	struct rthdr* req;
+
+	nc_header(&nc, command, flags, seq);
+
+	if(!(req = nc_fixed(&nc, sizeof(*req))))
+		goto send;
+
+	req->foo = ...
+	req->bar = ...
+
+	...
+send:
+	if((ret = nc_send(fd, &nc)) < 0)
+		fail(...);
+
+   Overflow, if happens, is handled in nc_send(). */
+
 struct nlattr;
 
 struct ncbuf {
@@ -21,15 +56,14 @@ void nc_put_str(struct ncbuf* nc, uint key, char* str);
 void nc_put_flag(struct ncbuf* nc, uint key);
 void nc_put_byte(struct ncbuf* nc, uint key, byte val);
 
-int nc_send(int fd, struct ncbuf* nc);
-
-int nc_recv_ack(int fd, void* buf, int len, int seq);
-
-int nn_recv(int fd, void* buf, int len);
-
 struct nlattr* nc_put_nest(struct ncbuf* nc, uint16_t type);
 void nc_end_nest(struct ncbuf* nc, struct nlattr* at);
 
-struct nlmsg* nc_msg(struct ncbuf* nc);
+int nc_send(int fd, struct ncbuf* nc);
 
-int nl_subscribe(int fd, int id);
+/* For debugging purposes, it may be useful to get the outbound message
+   as struct nlmsg (to pass it to nl_dump_genl for instance). Note just
+   casting nc->buf is not enough because the length of the message does
+   not get set until nc_send(). */
+
+struct nlmsg* nc_msg(struct ncbuf* nc);
--- lib/netlink/recv.h
+++ lib/netlink/recv.h
@@ -1,3 +1,24 @@
+/* NL shift buffer and related routines for receiving incoming
+   stream of messages from the kernel. Intended usage:
+
+	struct nrbuf nr;
+
+	nr_buf_set(&nr, buf, sizeof(buf));
+recv:
+	if((ret = nr_recv(fd, &nr)) < 0)
+		fail or return;
+next:
+	if(!(msg = nr_next(&nr)))
+		goto recv;
+	
+	handle(msg);
+
+	goto next;
+
+   nr_recv does a single sys_recv() and therefore can be used both
+   with blocking and non-blocking fds. The only difference is handling
+   EAGAIN return. */
+
 struct nrbuf {
 	void* buf;
 	void* msg;
@@ -10,3 +31,9 @@ int nl_recv(int fd, void* buf, int len);
 void nr_buf_set(struct nrbuf* nr, void* buf, unsigned len);
 int nr_recv(int fd, struct nrbuf* nr);
 struct nlmsg* nr_next(struct nrbuf* nr);
+
+/* Late (post-bind) subscription for event groups. Does a single ioctl
+   on the netlink socket. GENL event group ids are dynamic, are require
+   running commands through the socket before subscribing. */
+
+int nl_subscribe(int fd, int id);
