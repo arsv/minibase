19595ca8bf7affc3b5c142b96cae3cd093921a00
Alex Suykov <alex.suykov@gmail.com>
2019-12-26 21:55:30 +0200

    msh: simplify Makefile

--- src/msh/msh_cmd.sh
+++ src/msh/msh_cmd.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-cat <<END
+out=msh_cmd.h
+
+cat >$out <<END
 #ifndef CMD
 #define CMD(name) void cmd_##name(struct sh* ctx);
 #endif
 
 END
 
-sed -ne 's/^void cmd_\([a-z]\+\).*/CMD(\1)/p' msh_cmd_*.c | sort
+sed -ne 's/^void cmd_\([a-z]\+\).*/CMD(\1)/p' msh_cmd_*.c | sort >>$out
 
-cat <<END
+cat >>$out <<END
 
 #undef CMD
 END
