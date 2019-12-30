9ca4c4fb7510b89ad7315f8c3d97e7b45d70d3a7
Alex Suykov <alex.suykov@gmail.com>
2019-12-31 01:33:15 +0200

    lib: force 0777 permissions on nlusctl sockets

--- lib/nlusctl/addr_bind.c
+++ lib/nlusctl/addr_bind.c
@@ -1,6 +1,7 @@
 #include <bits/socket/unix.h>
 #include <sys/socket.h>
 #include <sys/fpath.h>
+#include <sys/creds.h>
 
 #include <nlusctl.h>
 #include "addr.h"
@@ -18,10 +19,29 @@
    If at some point sticky sockets become an option, this function should be
    removed. But that is not expected to happen soon, so for now, it's here. */
 
+/* Regarding umask use: in Linux, file permissions on local sockets do matter,
+   but handling them is quite difficult, and considering the planned security
+   model for this project, most likely pointless. It is pretty much decided
+   that permissions will be set (statically) on the directory instead. To make
+   that work, we force 0777 on sockets here, otherwise the missing bits would
+   change effective permissions.
+
+   There are no other ways of passing initial socket permissions in Linux
+   afaik. In pretty much all cases, current umask will NOT be 0000, in fact
+   using 0000 outside of this code is a pretty bad idea for most services.
+
+   Note in BSD things work exactly like that, without any umask calls, because
+   they just ignore file permissions on sockets. */
+
 int uc_listen(int fd, const char* path, int backlog)
 {
-	int ret;
+	int ret, mask = 0000;
 	struct sockaddr_un addr;
+
+	if((ret = sys_umask(mask)) < 0)
+		goto err;
+
+	mask = ret;
 
 	if((ret = uc_address(&addr, path)) < 0)
 		goto err;
@@ -37,6 +57,9 @@ int uc_listen(int fd, const char* path, int backlog)
 	if((ret = sys_bind(fd, &addr, sizeof(addr))) < 0)
 		goto err;
 got:
+	if((ret = sys_umask(mask)) < 0)
+		goto err;
+
 	ret = sys_listen(fd, backlog);
 err:
 	return ret;
