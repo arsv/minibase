f92ffc1a04a47597f98c7170b4def5e1d13365cd
Alex Suykov <alex.suykov@gmail.com>
2020-01-07 19:03:41 +0200

    rootsh: fix ctty handling
    Turns out you cannot re-use the same file block for TTIOCSCTTY.

--- src/vtms/rootsh.c
+++ src/vtms/rootsh.c
@@ -22,7 +22,7 @@ static int open_dev(char* tty)
 	p = fmtstr(p, e, tty);
 	FMTEND(p, e);
 
-	if((fd = sys_open(path, O_RDWR | O_CLOEXEC)) < 0)
+	if((fd = sys_open(path, O_RDWR | O_NOCTTY)) < 0)
 		fail(NULL, path, fd);
 
 	return fd;
@@ -32,14 +32,19 @@ static int child(int fd, char** args, char** envp)
 {
 	int ret;
 
-	sys_dup2(fd, 0);
-	sys_dup2(fd, 1);
-	sys_dup2(fd, 2);
+	if((ret = sys_dup2(fd, 0)) < 0)
+		fail("dup2", "stdin", ret);
+	if((ret = sys_dup2(fd, 1)) < 0)
+		fail("dup2", "stdout", ret);
+	if((ret = sys_dup2(fd, 2)) < 0)
+		fail("dup2", "stderr", ret);
 
 	if((ret = sys_setsid()) < 0)
 		fail("setsid", NULL, ret);
-	if((ret = sys_ioctli(STDOUT, TIOCSCTTY, 1)) < 0)
+	if((ret = sys_ioctli(fd, TIOCSCTTY, 0)) < 0)
 		fail("ioctl", "TIOCSCTTY", ret);
+	if((ret = sys_close(fd)) < 0)
+		fail("close", NULL, ret);
 
 	ret = execvpe(*args, args, envp);
 
@@ -53,9 +58,12 @@ static void sleep(int sec)
 	sys_nanosleep(&ts, NULL);
 }
 
-static void spawn_shell(int fd, char** args, char** envp)
+static void spawn_shell(char* tty, char** args, char** envp)
 {
-	int pid, ret, status;
+	int fd, pid, ret, status;
+
+	if((fd = open_dev(tty)) < 0)
+		fail("open", tty, fd);
 
 	if((pid = sys_fork()) < 0)
 		fail("fork", NULL, pid);
@@ -64,6 +72,8 @@ static void spawn_shell(int fd, char** args, char** envp)
 
 	if((ret = sys_waitpid(pid, &status, 0)) < 0)
 		fail("waitpid", NULL, ret);
+	if((ret = sys_close(fd)) < 0)
+		fail("close", tty, ret);
 
 	sleep(1);
 }
@@ -80,9 +90,7 @@ int main(int argc, char** argv)
 	if(strncmp(tty, "tty", 3))
 		fail("refusing to run on", tty, 0);
 
-	int fd = open_dev(tty);
-
 	char** args = argv + 2;
 
-	while(1) spawn_shell(fd, args, envp);
+	while(1) spawn_shell(tty, args, envp);
 }
