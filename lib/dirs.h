038ebcd0a726d9c0ec14db6966c343db818ae3f5
Alex Suykov <alex.suykov@gmail.com>
2020-02-10 17:21:56 +0200

    lib: SYS_ROOT dir for mpkg

--- lib/dirs.h
+++ lib/dirs.h
@@ -6,9 +6,11 @@
 # define BASE_ETC "./etc"
 # define BASE_VAR "./var"
 # define RUN_CTRL "./run"
+# define SYS_ROOT "."
 #else
 # define HERE ""
 # define BASE_ETC "/base/etc"
 # define BASE_VAR "/var/base"
 # define RUN_CTRL "/run/ctrl"
+# define SYS_ROOT "/"
 #endif
