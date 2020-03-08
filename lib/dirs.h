1ed0094c39386479abd126d671f4816973d2ef89
Alex Suykov <alex.suykov@gmail.com>
2020-03-08 07:17:09 +0200

    lib: INIT_ETC in dirs.h

--- lib/dirs.h
+++ lib/dirs.h
@@ -4,12 +4,14 @@
 #ifdef DEVEL
 # define HERE "."
 # define BASE_ETC "./etc"
+# define INIT_ETC "./etc"
 # define BASE_VAR "./var"
 # define RUN_CTRL "./run"
 # define SYS_ROOT "."
 #else
 # define HERE ""
 # define BASE_ETC "/base/etc"
+# define INIT_ETC "/etc"
 # define BASE_VAR "/var/base"
 # define RUN_CTRL "/run/ctrl"
 # define SYS_ROOT "/"
